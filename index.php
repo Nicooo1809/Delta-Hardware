838fe01653efc800004b8a6dfa38ecb7fd208fdf
Pflum <pflum@gmx.net>
2022-02-03 16:27:01 +0100

    Header angepasst

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-require_once("header.html");
+require_once("header.php");
 ?>
 
-
-HTML TEIL HIER!
-
+<video width="320" controls>
+  <source src="media/GeForce RTX 3080 Ti _ The New Gaming Flagship.mp4" type="video/mp4">
+  Your browser does not support the video tag.
+</video> 
 
 <?php
 require_once("footer.html");
