769bb95087b958f072867557540025b685717128
Pflum <pflum@gmx.net>
2022-02-04 13:20:23 +0100

    bugfix

--- logout.php
+++ logout.php
@@ -7,8 +7,8 @@ unset($_SESSION['userid']);
 setcookie("identifier","",time()-(3600*24*365)); 
 setcookie("securitytoken","",time()-(3600*24*365)); 
 
-require_once("inc/mysql.php");
-require_once("inc/functions.php");
+require_once("php/mysql.php");
+require_once("php/functions.php");
 
 header("location: index.php");
 exit();
