5fe741471f59e678f34942991e17198b7f5a28f7
Pflum <pflum@gmx.net>
2022-02-04 08:06:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once("templates/header.php");
 ?>
 
-<video style="object-fit: contain;" autoplay muted loop>
+<video style="height: 100vh; width: 100%; object-fit: fill; position: absolute;" autoplay muted loop>
   <source src="media/GeForce RTX 3080 Ti _ The New Gaming Flagship.mp4" type="video/mp4">
   Your browser does not support the video tag.
 </video> 
