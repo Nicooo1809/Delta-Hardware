32964b6ed5fc6abccc2b8c2932952b3e27410c53
Pflum <pflum@gmx.net>
2022-02-16 18:49:31 +0100

    Update products.php

--- products.php
+++ products.php
@@ -28,26 +28,25 @@ $stmt->bindValue(2, $num_products_on_each_page, PDO::PARAM_INT);
 $stmt->execute();
 // Fetch the products from the database and return the result as an Array
 $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$stmt->debugDumpParams();
+#$stmt->debugDumpParams();
 // Get the total number of products
 $total_products = $pdo->query('SELECT * FROM products, products_types where products.product_id = products_types.id ' . $type)->rowCount();
 ?>
 
 <div class="products content-wrapper">
     <h1>Products</h1>
-    <form action="products.php?<?php print(http_build_query($_GET)); ?>" method="get">
-        <select name="sortby" id="sortby">
-            <option value="name">Name</option>
-            <option value="price">Preis</option>
-            <option value="rrp">UVP</option>
-            <option value="created_at">Date</option>
-        </select>
-    <input type="Submit" value="Aufsteigend" id="order" name="order"></input>
-    <input type="Submit" value="Absteigend" id="order" name="order"></input>
-    </form>
+    <!--form action="products.php?<?php print(http_build_query($_GET)); ?>" method="get"-->
+    <select name="sortby" id="sortby">
+        <option value="name">Name</option>
+        <option value="price">Preis</option>
+        <option value="rrp">UVP</option>
+        <option value="created_at">Date</option>
+    </select>
+    <input type="Submit" value="Aufsteigend" id="order" name="order" href="products.php?order=Aufsteigend&<?php print(http_build_query($_GET)); ?>"></input>
+    <input type="Submit" value="Absteigend" id="order" name="order" href="products.php?order=Absteigend&<?php print(http_build_query($_GET)); ?>"></input>
+    <!--/form-->
     <p><?php print($total_products); ?> Products</p>
     <div class="products-wrapper">
-    <?php print_r($products); ?>
         <?php foreach ($products as $product): ?>
         <a href="product.php?id=<?=$product['id']?>" class="product">
             <img src="imgs/<?=$product['img']?>" width="200" height="200" alt="<?=$product['name']?>">
