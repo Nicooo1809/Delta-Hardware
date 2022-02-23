977df85a2130560e4ad26a96ada3f0fe36bd8d94
Pflum <pflum@gmx.net>
2022-02-23 09:32:05 +0100

    Create product.php

---
+++ product.php
@@ -0,0 +1,40 @@
+<?php
+require_once("php/mysql.php");
+require_once("php/functions.php");
+require "templates/header.php";
+// The amounts of products to show on each page
+$num_products_on_each_page = 4;
+// The current page, in the URL this will appear as index.php?page=products&p=1, index.php?page=products&p=2, etc...
+$current_page = isset($_GET['p']) && is_numeric($_GET['p']) ? (int)$_GET['p'] : 1;
+if (!isset($_SESSION["id"])) {
+    header("location: products.php");
+}
+// Select products ordered by the date added
+$stmt = $pdo->prepare('SELECT * FROM products where id = ?');
+// bindValue will allow us to use integer in the SQL statement, we need to use for LIMIT
+$stmt->bindValue(1, $_SESSION["id"], PDO::PARAM_INT);
+$stmt->execute();
+// Fetch the products from the database and return the result as an Array
+$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
+#print_r($products);
+#$stmt->debugDumpParams();
+?>
+<div class="products content-wrapper">
+    <h1><?=$product['name']?></h1>
+    <p><?php print($total_products); ?> Products</p>
+    <div class="products-wrapper">
+        <a href="product.php?id=<?=$product['id']?>" class="product">
+            <img src="product_img/<?=$product['img']?>" width="200" height="200" alt="<?=$product['name']?>">
+            <span class="name"><?=$product['name']?></span>
+            <span class="price">
+                &dollar;<?=$product['price']?>
+                <?php if ($product['rrp'] > 0): ?>
+                <span class="rrp">&dollar;<?=$product['rrp']?></span>
+                <?php endif; ?>
+            </span>
+        </a>
+    </div>
+</div>
+<?php
+require "templates/footer.html";
+?>
