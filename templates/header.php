3fc8a609b3ec803d20cef19991ed917db95b0b14
Nicooo <niggoapfel@gmail.com>
2022-02-25 14:40:26 +0100

    Header update

--- templates/header.php
+++ templates/header.php
@@ -14,6 +14,7 @@ session_start();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/3386a0b16e.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
     <title>Delta-Hardware</title>
 </head>
 <body>
@@ -49,6 +50,8 @@ session_start();
                 <input class="form-control me-2" name="search" type="search" placeholder="Grafikkarte..." aria-label="Suche">
                 <button class="btn btn-outline-success" type="submit">Suchen</button>
             </form>
+            </i>
+            <i class="bi-person-fill" style="font-size: 2rem; color: #ffffff;" href="<?php if(isset($_SESSION['userid'])) {print("settings.php");} else {print("login.php");} ?>"></i>
         </div>
     </div>
 </nav>
