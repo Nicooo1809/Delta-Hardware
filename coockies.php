c9976660ed41ab346f29607831abbf584c09d152
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-03 20:48:10 +0100

    Update coockies.php

--- coockies.php
+++ coockies.php
@@ -1,11 +1,14 @@
 <?php
 include_once('templates/header.php');
 ?>
-<h1>Cookies</h1>
-<p>Cookies wird nur verwendet für:</p>
-<li>Das speichern der PHP Session</li>
-<li>Login Remember me Funktion</li>
-<li>Das Speichern der Cookie Einstellung</li>
+<div>
+    <h1 class="text-white">Cookies</h1>
+    <p class="text-white">Wir verwenden nur die Cookies die die Webseite zum Funktionieren benötigt, nämlich:</p>
+    <li>Das speichern der PHP Session</li>
+    <li>Login Remember me Funktion</li>
+    <li>Das Speichern der Cookie Einstellung</li>
+</div>
+    
 <?php
 include_once('templates/footer.php');
 ?>
