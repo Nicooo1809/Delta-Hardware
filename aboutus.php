18e8b6edb5afa84a376854f3a51eafce2654c6c4
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-03 21:30:42 +0100

    About us test stuff

---
+++ aboutus.php
@@ -0,0 +1,23 @@
+<?php 
+require_once("templates/header.php");
+?>
+
+<div class="mx-auto">
+    <div class="text-white">
+        <h1>Wer sind wir?</h1>
+        <p>
+            Wir sind Schüler der Berufsschule <a href="https://its-stuttgart.de/">it.schule Stuttgart</a>.
+            Um genau zu sein sind wir die Projektgruppe Delta, diese Webseite stellt unser BfK-S Projekt für die Klasse E2FS3BT dar.
+        </p>
+    </div>
+    
+        
+
+
+
+</div>
+
+
+<?php
+require_once("templates/footer.html");
+?>
