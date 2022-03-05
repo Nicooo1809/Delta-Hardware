4356e88b6c3042cbd20d25f37bcc2ac61fb76dd7
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-05 16:31:15 +0100

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -3,9 +3,6 @@ require_once("templates/header.php");
 ?>
 
 <?php
-function isMobile () {
-    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
-  }
 if(!isMobile()):
 ?>
 
