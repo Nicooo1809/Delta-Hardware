6dbf9671037405da10de9e24d8fffe5778def0b4
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-05 16:03:41 +0100

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -26,18 +26,7 @@
     </footer>
 
 
-    <footer class="footer fixed-bottom py-3 bg-dark">
-        <div class="container-fluid">
-            <div class="text-white">
-                <div class="col text-center">
-                    <a href="aboutus.php" class="text-white">Über uns</a>
-                </div>
-                <div class="col me-2 text-end">
-                    Version 0.0.3 - 05.03.2022
-                </div>
-            </div>
-        </div>
-    </footer>
+
 
 
 </body>
