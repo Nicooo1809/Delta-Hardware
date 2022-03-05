690f679aa180286afd82e310cacc2d7f97135bb2
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-05 16:43:03 +0100

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -20,7 +20,7 @@ if(!isMobile()):
 
 <!-- Mobile Devices -->
 <?php else:?>
-    <div class="mx-auto my-3 py-2 px-3 text-center rounded bg-dark" style="width: 90%;">
+    <div class="mx-auto my-3 mx-3 py-2 px-3 text-center rounded bg-dark">
     <div class="text-white">
         <h1>Wer sind wir?</h1>
         <p>
