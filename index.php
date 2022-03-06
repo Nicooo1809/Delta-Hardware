9d955ed0cbbb23fefd12849dfd41fc66650faa78
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-06 17:12:19 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,20 +2,21 @@
 require_once("templates/header.php");
 ?>
 
-
-<div
-  class="bg-image p-5 text-center shadow-1-strong rounded mb-5 text-white"
-  style="background-image: url('/media/RTX_Showcase.jpg');"
->
-
-  <h1 class="mb-3 h2">Lorem ipsum</h1>
-
-  <p>
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus praesentium
-    labore accusamus sequi, voluptate debitis tenetur in deleniti possimus modi voluptatum
-    neque maiores dolorem unde? Aut dolorum quod excepturi fugit.
-  </p>
+<div class="container-fluid">
+  <div class="row no-gutter">
+    <div class="bg-image p-5 text-center shadow-1-strong rounded mb-5 text-white" style="background-image: url('/media/RTX_Showcase.jpg');">
+      <h1 class="mb-3 h2">Lorem ipsum</h1>
+      <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus praesentium
+          labore accusamus sequi, voluptate debitis tenetur in deleniti possimus modi voluptatum
+          neque maiores dolorem unde? Aut dolorum quod excepturi fugit.
+      </p>
+    </div>
+  </div>
 </div>
+
+
+
 
 <?php
 include_once("templates/footer.php")
