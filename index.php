6136513684c940ea9c27004d2969736e7f35d2a7
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-06 17:30:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 require_once("templates/header.php");
 ?>
 
-<div class="container-fluid">
-  <div class="row no-gutter">
+<div class="container-fluid h-100">
+  <div class="row justify-content-center h-100">
     <div class="bg-image p-5 text-center shadow-1-strong rounded mb-5 text-white" style="background-image: url('/media/RTX_Showcase.jpg');">
       <h1 class="mb-3 h2">Lorem ipsum</h1>
       <p>
@@ -11,7 +11,7 @@ require_once("templates/header.php");
           labore accusamus sequi, voluptate debitis tenetur in deleniti possimus modi voluptatum
           neque maiores dolorem unde? Aut dolorum quod excepturi fugit.
       </p>
-      <div class="full-height"></div>
+      <div class="row justify-content-center flex-grow-1">
     </div>
   </div>
 </div>
