1f950153096b6e17134e64140146721c79cd3660
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-08 18:18:06 +0100

    Update error.php

--- templates/error.php
+++ templates/error.php
@@ -1,4 +1,6 @@
 <div class="minheight100 container-fluid position-absolute top-50 start-50 translate-middle">
-    <h1 class="text-danger">Fehler</h1>
-    <p class="text-white"><?php echo $error_msg;?></p>
+    <div class="position-absolute top-50 start-50 translate-middle">
+        <h1 class="text-danger">Fehler</h1>
+        <p class="text-white"><?php echo $error_msg;?></p>
+    </div>
 </div>
