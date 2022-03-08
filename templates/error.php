52173e03f284f59d162f30bb3e40ae1ca65f0b4d
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-08 18:20:49 +0100

    Update error.php

--- templates/error.php
+++ templates/error.php
@@ -1,5 +1,5 @@
 <div class="minheight100 container-fluid position-absolute top-50 start-50 translate-middle">
-    <div class="position-absolute top-50 start-50 translate-middle">
+    <div class="d-flex align-items-center">
         <h1 class="text-danger">Fehler</h1>
         <p class="text-white"><?php echo $error_msg;?></p>
     </div>
