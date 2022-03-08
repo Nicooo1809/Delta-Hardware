05fa31340dc065727dc3b433381955f8ca1755ff
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-08 18:15:33 +0100

    Update error.php

--- templates/error.php
+++ templates/error.php
@@ -1,5 +1,4 @@
-<div class="container">
-<h2>Fehler</h2>
-
-<?php echo $error_msg; ?>
+<div class="minheight100 container-fluid position-absolute top-50 start-50 translate-middle">
+    <h1 class="text-danger">Fehler</h1>
+    <p class="text-white"><?php echo $error_msg;?></p>
 </div>
