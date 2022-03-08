6eefacd3441b0cde6b316ea8dc22ee2aee267624
Pflum <pflum@gmx.net>
2022-03-08 22:10:56 +0100

    Update read.php

--- admin/read.php
+++ admin/read.php
@@ -1,19 +1,11 @@
 <?php
 chdir('..');
 require_once("php/functions.php");
-// Get the page via GET request (URL param: page), if non exists default the page to 1
-$page = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
-// Number of records to show on each page
-$records_per_page = 5;
 // Prepare the SQL statement and get records from our contacts table, LIMIT will determine the page
-$stmt = $pdo->prepare('SELECT * FROM contacts ORDER BY id LIMIT :current_page, :record_per_page');
-$stmt->bindValue(':current_page', ($page-1)*$records_per_page, PDO::PARAM_INT);
-$stmt->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
+$stmt = $pdo->prepare('SELECT * FROM contacts ORDER BY id');
 $stmt->execute();
 // Fetch the records so we can display them in our template.
 $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// Get the total number of contacts, this is so we can determine whether there should be a next and previous button
-$num_contacts = $pdo->query('SELECT COUNT(*) FROM contacts')->fetchColumn();
 require_once("templates/header.php");
 ?>
 
@@ -49,14 +41,6 @@ require_once("templates/header.php");
             <?php endforeach; ?>
         </tbody>
     </table>
-	<div class="pagination">
-		<?php if ($page > 1): ?>
-		<a href="read.php?page=<?=$page-1?>"><i class="fas fa-angle-double-left fa-sm"></i></a>
-		<?php endif; ?>
-		<?php if ($page*$records_per_page < $num_contacts): ?>
-		<a href="read.php?page=<?=$page+1?>"><i class="fas fa-angle-double-right fa-sm"></i></a>
-		<?php endif; ?>
-	</div>
 </div>
 <?php
 include_once("templates/footer.php")
