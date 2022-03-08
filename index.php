78b9a07ae6b5c6ab7567d03d68ec8634e70a68f8
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-08 16:23:27 +0100

    index

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 require_once("templates/header.php");
 ?>
 
+
+<div class="minheight100" style="background-image: url('/media/RTX_Showcase.jpg');">
+</div>
+<!--
 <div class="minheight100 bg-image p-5 text-center shadow-1-strong rounded mb-5 text-white" style="background-image: url('/media/RTX_Showcase.jpg');">
   <h1 class="mb-3 h2">Lorem ipsum</h1>
   <p>
@@ -10,6 +14,7 @@ require_once("templates/header.php");
       neque maiores dolorem unde? Aut dolorum quod excepturi fugit.
   </p>
 </div>
+-->
 
 <?php
 include_once("templates/footer.php")
--- product.php
+++ product.php
@@ -96,7 +96,7 @@ $images = $stmt->fetchAll(PDO::FETCH_ASSOC);
                         <span class="rrp text-white">UVP &euro;<?=$product[0]['rrp']?></span>
                         <?php endif; ?>
                         <?php if ($product[0]['quantity'] <= 5): ?>
-                            <i class="fa-solid fa-exclamation"></i><span class="amount"> Nur noch <?=$product[0]['quantity']?> auf Lager, jetzt bestellen</span>
+                            <i class="fa-solid fa-exclamation"></i><span class="text-white"> Nur noch <?=$product[0]['quantity']?> auf Lager, jetzt bestellen</span>
                         <?php endif; ?>
                     </span>
                     <span class="desc text-white"><?=$product[0]['desc']?></span>
