e9e0229ce2fd5ac19230116085be92c31d9fc500
Pflum <pflum@gmx.net>
2022-03-08 22:47:32 +0100

    Update delete.php

--- admin/user/delete.php
+++ admin/user/delete.php
@@ -5,7 +5,7 @@ $msg = '';
 // Check that the contact ID exists
 if (isset($_GET['id'])) {
     // Select the record that is going to be deleted
-    $stmt = $pdo->prepare('SELECT * FROM contacts WHERE id = ?');
+    $stmt = $pdo->prepare('SELECT * FROM users WHERE id = ?');
     $stmt->execute([$_GET['id']]);
     $contact = $stmt->fetch(PDO::FETCH_ASSOC);
     if (!$contact) {
@@ -15,7 +15,7 @@ if (isset($_GET['id'])) {
     if (isset($_GET['confirm'])) {
         if ($_GET['confirm'] == 'yes') {
             // User clicked the "Yes" button, delete record
-            $stmt = $pdo->prepare('DELETE FROM contacts WHERE id = ?');
+            $stmt = $pdo->prepare('DELETE FROM users WHERE id = ?');
             $stmt->execute([$_GET['id']]);
             $msg = 'You have deleted the contact!';
         } else {
