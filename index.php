c173ef2889a406fff2a2df1d380ecb41989adf45
Nicooo <niggoapfel@gmail.com>
2022-03-09 11:01:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,11 +6,15 @@ require_once("templates/header.php");
     <div class="modal-dialog bg-dark">
         <div class="modal-content bg-dark">
             <div class="modal-header bg-dark">
-                <h4 class="modal-title text-white fw-bold" id="cookieModalLabel">Mhhh Lecker...</h4>
+                <h4 class="modal-title text-white fw-bold" id="cookieModalLabel">Mhhh Lecker &#x1F36A;!</h4>
                 <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body text-white bg-dark fw-normal">
-                Test
+                <div class="px-2">
+                    <li>Das speichern der PHP Session</li>
+                    <li>Login Remember me Funktion</li>
+                    <li>Das Speichern der Cookie Einstellung</li>
+                </div>
             </div>
             <div class="modal-footer text-white bg-dark fw-bold">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Schließen</button>
