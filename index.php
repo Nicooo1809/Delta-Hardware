83460e1ff9b1f672011314c69cc07f5827ccc2b5
Nicooo <niggoapfel@gmail.com>
2022-03-09 08:24:08 +0100

    update index.php

--- index.php
+++ index.php
@@ -3,19 +3,9 @@ require_once("templates/header.php");
 ?>
 
 
-<div class="minheight100 bg-img">
-<div style="width: fit-content;"></div>
+<div class="bg-image">
+
 </div>
-<!--
-<div class="minheight100 bg-image p-5 text-center shadow-1-strong rounded mb-5 text-white" style="background-image: url('/media/RTX_Showcase.jpg');">
-  <h1 class="mb-3 h2">Lorem ipsum</h1>
-  <p>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus praesentium
-      labore accusamus sequi, voluptate debitis tenetur in deleniti possimus modi voluptatum
-      neque maiores dolorem unde? Aut dolorum quod excepturi fugit.
-  </p>
-</div>
--->
 
 <?php
 include_once("templates/footer.php")
