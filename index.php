503d1267df8a2d50c906f8b104c07b251458ef15
Nicooo <niggoapfel@gmail.com>
2022-03-09 21:01:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -30,26 +30,23 @@ require_once("templates/header.php");
 
 
 <main>
-    
-</main>
-
-<div class="minheight100">
-</div>
-
-
-<div class="view bg">
-    <div class="mask rgba-black-light align-items-center">
-        <div class="container">
-            <div class="row min-vh-100 justify-content-center align-items-center">
-                <div class="col-md-12 mt-1 white-text text-center">
-                    <h1 class="h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5"><strong>Die Neue RTX 3070 Ti</strong></h1>   
-                    <hr class="hr-light my-3">
-                    <h5 class="text-uppercase mb-4 white-text"><strong>Jetzt kaufen!</strong></h5>
+    <div class="view bg">
+        <div class="mask rgba-black-light align-items-center">
+            <div class="container">
+                <div class="row min-vh-100 justify-content-center align-items-center">
+                    <div class="col-md-12 mt-1 white-text text-center">
+                        <h1 class="h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5"><strong>Die Neue RTX 3070 Ti</strong></h1>   
+                        <hr class="hr-light my-3">
+                        <h5 class="text-uppercase mb-4 white-text"><strong>Jetzt kaufen!</strong></h5>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div> 
+    </div> 
+</main>
+
+
+
 
 
 
