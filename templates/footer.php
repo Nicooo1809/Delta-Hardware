f3059d4ebe1dd4df233303489995c13ff261047c
Nicooo <niggoapfel@gmail.com>
2022-03-09 09:42:05 +0100

    Modal update

--- templates/footer.php
+++ templates/footer.php
@@ -4,25 +4,6 @@
     <button type="button" class="btn btn-primary btn-sm acceptcookies">
         Ich stimme zu
     </button>
-
-
-    <div class="modal fade" id="cookieModal" tabindex="-1" aria-labelledby="cookieModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="cookieModalLabel">Mhhh lecker...</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    Test
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
   
 <script src="/js/cookies.js"></script>
 
