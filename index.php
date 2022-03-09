3a3462cd054dab1b725524d825c5dfc3ecdd66f5
Nicooo <niggoapfel@gmail.com>
2022-03-09 10:09:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,15 +5,16 @@ require_once("templates/header.php");
 <div class="modal fade" id="cookieModal" tabindex="-1" aria-labelledby="cookieModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="cookieModalLabel">Mhhh Lecker test</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            Test
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Ok</button>
+            <div class="modal-header">
+                <h5 class="modal-title" id="cookieModalLabel">Mhhh Lecker test</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Test
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Ok</button>
+            </div>
         </div>
     </div>
 </div>
