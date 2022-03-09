51a91dc4fe8e91fec10f68ead48ab765c7dc10e9
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-09 10:39:09 +0100

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -3,16 +3,20 @@ require_once("templates/header.php")
 ?>
 
 <!-- Desktop Devices -->
-<div class="container minheight100 row my-3 py-3 px-3 text-center">
-    <div class="col">
-        <div class="card bg-dark" style="width: 50rem;">
-            <div class="card-body">
-                <h1 class="card-title my-0">Wer sind wir?</h1>
-                <p class="card-text">
-                    Wir sind Schüler*innen der <a href="https://its-stuttgart.de/">it.schule</a> Stuttgart.<br>
-                    Genau genommen sind wir die Projektgruppe Delta, welche an der erstellung eines Hardware Webshops arbeitet. <br>
-                    Dieser Webshop wird das Produkt unserer BfK-S Projektarbeit in der Klasse E2FS3BT sein.
-                </p>
+<div class="container minheight100 my-3 py-3 px-3">
+    <div class="row no-gutter">
+        <div class="d-flex align-items-center">
+            <div class="col">
+                <div class="card bg-dark" style="width: 50rem;">
+                    <div class="card-body">
+                        <h1 class="card-title my-0 text-center">Wer sind wir?</h1>
+                        <p class="card-text text-center">
+                            Wir sind Schüler*innen der <a href="https://its-stuttgart.de/">it.schule</a> Stuttgart.<br>
+                            Genau genommen sind wir die Projektgruppe Delta, welche an der erstellung eines Hardware Webshops arbeitet. <br>
+                            Dieser Webshop wird das Produkt unserer BfK-S Projektarbeit in der Klasse E2FS3BT sein.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
