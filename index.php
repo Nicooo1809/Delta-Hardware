e0dc0be7b60ad3b00399dedf9e3ff69a5c06fe60
Nicooo <niggoapfel@gmail.com>
2022-03-09 10:17:57 +0100

    cookie modal

--- index.php
+++ index.php
@@ -4,15 +4,15 @@ require_once("templates/header.php");
 
 <div class="modal fade" id="cookieModal" tabindex="-1" aria-labelledby="cookieModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-        <div class="modal-content bg-dark">
-            <div class="modal-header bg-dark">
+        <div class="modal-content">
+            <div class="modal-header">
                 <h5 class="modal-title" id="cookieModalLabel">Mhhh Lecker...</h5>
                 <button type="button" class="btn-close text-white" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body bg-dark">
+            <div class="modal-body">
                 Test
             </div>
-            <div class="modal-footer bg-dark">
+            <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Ok</button>
             </div>
         </div>
