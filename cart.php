6ad6d56066d3864d094d5698e6b9d4005513bd73
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-09 22:00:56 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -192,8 +192,6 @@ foreach ($products as $product) {
                                             <input class="form-control" type="number" value="<?=$product['quantity']?>" min="1" max="<?=$product['maxquantity']?>" class="form-control form-control-sm" name="quantity" required>
                                             <button type="submit" name="action" value="mod" class="btn btn-outline-primary">Speichern</button>
                                         </div>
-                                    </form>
-                                    <form action="cart.php" method="post">
                                         <input type="number" value="<?=$product['id']?>" name="listid" style="display: none;" required>
                                         <button type="submit" name="action" value="del" class="btn btn-outline-primary">Löschen</button>
                                     </form>
