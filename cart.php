425abefd5b6c0eca5960839f42ebf49bd5c698f0
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-10 07:57:18 +0100

    Update cart.php

--- cart.php
+++ cart.php
@@ -76,12 +76,39 @@ if(isset($_POST['action'])) {
             } else {
                 require_once("templates/header.php");
                 ?>
-                <form action="cart.php" method="post">
-                    <input type="number" value="<?=$_POST['listid']?>" name="listid" style="display: none;" required>
-                    <input type="text" value="del" name="action" style="display: none;" required>
-                    <button class="btn btn-outline-primary" type="submit" name="confirm" value="yes">Yes</button>
-                    <button class="btn btn-outline-primary" type="submit" name="confirm" value="no">No</button>
-                </form>
+                <div class="minheight100">
+                    <div class="modal fade" id="cartModal" tabindex="-1" aria-labelledby="cartModalLabel" aria-hidden="true">
+                        <div class="modal-dialog bg-dark">
+                            <div class="modal-content bg-dark">
+                                <div class="modal-header bg-dark">
+                                    <h4 class="modal-title text-white fw-bold" id="cartModalLabel">Mhhh Lecker &#x1F36A;!</h4>
+                                    <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <hr>
+                                <div class="modal-body text-white bg-dark fw-normal">
+                                    <div class="px-2">
+                                        <h4 class="fw-bold">Wir verwenden Cookies um folgende Funktion bereitzustellen:</h4>
+                                        <br>
+                                        <p class="fs-5 cookie-p-text">- Speichern der PHP-Session</p>
+                                        <p class="fs-5 cookie-p-text">- Angemeldet bleiben</p>
+                                        <p class="fs-5 cookie-p-text mb-1">- Speichern der Cookie-Einstellung</p>
+                                        <br>
+                                        <p class="fw-light fs-6 cookie-p-text">Ihre Cookie-Einstellung wird gespeichert.</p>
+                                    </div>
+                                </div>
+                                <div class="modal-footer text-white bg-dark fw-bold">
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Schließen</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <form action="cart.php" method="post">
+                        <input type="number" value="<?=$_POST['listid']?>" name="listid" style="display: none;" required>
+                        <input type="text" value="del" name="action" style="display: none;" required>
+                        <button class="btn btn-outline-primary" type="submit" name="confirm" value="yes">Yes</button>
+                        <button class="btn btn-outline-primary" type="submit" name="confirm" value="no">No</button>
+                    </form>
+                </div>
                 <?php
                 require_once("templates/footer.php");
                 exit;
