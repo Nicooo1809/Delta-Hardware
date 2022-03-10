7286d3cb9565426ad061374232aa15c86fde5996
Pflum <pflum@gmx.net>
2022-03-10 12:55:50 +0100

    Update user.php

--- user.php
+++ user.php
@@ -87,11 +87,15 @@ if(isset($_POST['action'])) {
                     $stmt->bindValue(2, $_POST['userid'], PDO::PARAM_INT);
                     $stmt->execute();
                 }
-                if (isset($_POST['permissions']) and !empty($_POST['permissions'])) {
-                    $stmt = $pdo->prepare("UPDATE users SET permission_group = ?, updated_at = now() WHERE users.id = ?");
-                    $stmt->bindValue(1, $_POST['permissions'], PDO::PARAM_INT);
-                    $stmt->bindValue(2, $_POST['userid'], PDO::PARAM_INT);
-                    $stmt->execute();
+                if ($user['modifyUserPerms'] != 1) {
+                    error('Permission denied!');
+                } else {
+                    if (isset($_POST['permissions']) and !empty($_POST['permissions'])) {
+                        $stmt = $pdo->prepare("UPDATE users SET permission_group = ?, updated_at = now() WHERE users.id = ?");
+                        $stmt->bindValue(1, $_POST['permissions'], PDO::PARAM_INT);
+                        $stmt->bindValue(2, $_POST['userid'], PDO::PARAM_INT);
+                        $stmt->execute();
+                    }
                 }
                 #error_log(pdo_debugStrParams($stmt));
                 header("location: user.php");
