20ff3519aecaff98f40ed4c2f3642699f28e0c3c
Pflum <pflum@gmx.net>
2022-03-10 08:38:04 +0100

    Update user.php

--- user.php
+++ user.php
@@ -4,48 +4,48 @@ $user = check_user();
 
 if(isset($_POST['action'])) {
     if($_POST['action'] == 'add') {
-        if(isset($_POST['productid']) and isset($_POST['quantity']) and !empty($_POST['productid']) and !empty($_POST['quantity'])) {
+        if(isset($_POST['user1id']) and isset($_POST['quantity']) and !empty($_POST['user1id']) and !empty($_POST['quantity'])) {
 
-            $stmt = $pdo->prepare('SELECT *, products.quantity as maxquantity FROM products, product_list where product_list.product_id = products.id and product_id = ? and products.id in (SELECT product_id FROM product_list where list_id = (select id from orders where kunden_id = ? and ordered = 0 and sent = 0)) and product_list.list_id = (select id from orders where kunden_id = ? and ordered = 0 and sent = 0)');
-            $stmt->bindValue(1, $_POST['productid'], PDO::PARAM_INT);
+            $stmt = $pdo->prepare('SELECT *, users.quantity as maxquantity FROM users, user1_list where user1_list.user1_id = users.id and user1_id = ? and users.id in (SELECT user1_id FROM user1_list where list_id = (select id from orders where kunden_id = ? and ordered = 0 and sent = 0)) and user1_list.list_id = (select id from orders where kunden_id = ? and ordered = 0 and sent = 0)');
+            $stmt->bindValue(1, $_POST['user1id'], PDO::PARAM_INT);
             $stmt->bindValue(2, $user['id'], PDO::PARAM_INT);
             $stmt->bindValue(3, $user['id'], PDO::PARAM_INT);
             $stmt->execute();
-            $product = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            #error_log(print_r($product, true));
+            $user1 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            #error_log(print_r($user1, true));
 
-            if (isset($product[0])) {
-                if ($_POST['quantity'] + $product[0]['quantity'] > $product[0]['maxquantity']) {
-                    $quantity = $product[0]['maxquantity'];
+            if (isset($user1[0])) {
+                if ($_POST['quantity'] + $user1[0]['quantity'] > $user1[0]['maxquantity']) {
+                    $quantity = $user1[0]['maxquantity'];
                 } else {
-                    $quantity = $_POST['quantity'] + $product[0]['quantity'];
+                    $quantity = $_POST['quantity'] + $user1[0]['quantity'];
                 }
                 if ($quantity < 1) {
                     $quantity = 1;
                 }
-                $stmt = $pdo->prepare('UPDATE product_list SET quantity = ? WHERE id = ?');
+                $stmt = $pdo->prepare('UPDATE user1_list SET quantity = ? WHERE id = ?');
                 $stmt->bindValue(1, $quantity, PDO::PARAM_INT);
-                $stmt->bindValue(2, $product[0]['id'], PDO::PARAM_INT);
+                $stmt->bindValue(2, $user1[0]['id'], PDO::PARAM_INT);
                 $stmt->execute();
                 header("location: cart.php");
                 exit;
             } else {
-                $stmt = $pdo->prepare('SELECT * FROM products where products.id = ?');
-                $stmt->bindValue(1, $_POST['productid'], PDO::PARAM_INT);
+                $stmt = $pdo->prepare('SELECT * FROM users where users.id = ?');
+                $stmt->bindValue(1, $_POST['user1id'], PDO::PARAM_INT);
                 $stmt->execute();
-                $product = $stmt->fetchAll(PDO::FETCH_ASSOC);
-                #print_r($product);
-                if ($_POST['quantity'] > $product[0]['quantity']) {
-                    $quantity = $product[0]['quantity'];
+                $user1 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                #print_r($user1);
+                if ($_POST['quantity'] > $user1[0]['quantity']) {
+                    $quantity = $user1[0]['quantity'];
                 } else {
                     $quantity = $_POST['quantity'];
                 }
                 if ($quantity < 1) {
                     $quantity = 1;
                 }
-                $stmt = $pdo->prepare('INSERT INTO product_list (list_id, product_id, quantity) VALUES ((select id from orders where kunden_id = ? and ordered = 0 and sent = 0), ?, ?)');
+                $stmt = $pdo->prepare('INSERT INTO user1_list (list_id, user1_id, quantity) VALUES ((select id from orders where kunden_id = ? and ordered = 0 and sent = 0), ?, ?)');
                 $stmt->bindValue(1, $user['id'], PDO::PARAM_INT);
-                $stmt->bindValue(2, $_POST['productid']);
+                $stmt->bindValue(2, $_POST['user1id']);
                 $stmt->bindValue(3, $quantity, PDO::PARAM_INT);
                 $stmt->execute();
                 header("location: cart.php");
@@ -58,12 +58,12 @@ if(isset($_POST['action'])) {
         }
     }
     if($_POST['action'] == 'del') {
-        if(isset($_POST['listid']) and !empty($_POST['listid'])) {
+        if(isset($_POST['userid']) and !empty($_POST['userid'])) {
             if (isset($_POST['confirm']) and !empty($_POST['confirm'])) {
                 if ($_POST['confirm'] == 'yes') {
                     // User clicked the "Yes" button, delete record
-                    $stmt = $pdo->prepare('DELETE FROM product_list WHERE id = ? and list_id = (select id from orders where kunden_id = ? and ordered = 0 and sent = 0)');
-                    $stmt->bindValue(1, $_POST['listid'], PDO::PARAM_INT);
+                    $stmt = $pdo->prepare('DELETE FROM user1_list WHERE id = ? and list_id = (select id from orders where kunden_id = ? and ordered = 0 and sent = 0)');
+                    $stmt->bindValue(1, $_POST['userid'], PDO::PARAM_INT);
                     $stmt->bindValue(2, $user['id'], PDO::PARAM_INT);
                     $stmt->execute();
                     header('Location: cart.php');
@@ -85,7 +85,7 @@ if(isset($_POST['action'])) {
                                         <p class="text-center">
                                             <div>
                                             <form action="cart.php" method="post">
-                                                <input type="number" value="<?=$_POST['listid']?>" name="listid" style="display: none;" required>
+                                                <input type="number" value="<?=$_POST['userid']?>" name="userid" style="display: none;" required>
                                                 <input type="text" value="del" name="action" style="display: none;" required>
                                                 <button class="btn btn-outline-primary mx-2" type="submit" name="confirm" value="yes">Ja</button>
                                                 <button class="btn btn-outline-primary mx-2" type="submit" name="confirm" value="no">Nein</button>
@@ -105,22 +105,22 @@ if(isset($_POST['action'])) {
         }
     }
     if($_POST['action'] == 'mod') {
-        if(isset($_POST['listid']) and !empty($_POST['listid'])) {
-            $stmt = $pdo->prepare('select *, products.quantity as maxquantity from products, product_list where products.id = product_list.product_id and product_list.id = ?');
-            $stmt->bindValue(1, $_POST['listid'], PDO::PARAM_INT);
+        if(isset($_POST['userid']) and !empty($_POST['userid'])) {
+            $stmt = $pdo->prepare('select *, users.quantity as maxquantity from users, user1_list where users.id = user1_list.user1_id and user1_list.id = ?');
+            $stmt->bindValue(1, $_POST['userid'], PDO::PARAM_INT);
             $stmt->execute();
-            $product = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            if ($_POST['quantity'] > $product[0]['maxquantity']) {
-                $quantity = $product[0]['maxquantity'];
+            $user1 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            if ($_POST['quantity'] > $user1[0]['maxquantity']) {
+                $quantity = $user1[0]['maxquantity'];
             } else {
                 $quantity = $_POST['quantity'];
             }
             if ($quantity < 1) {
                 $quantity = 1;
             }
-            $stmt = $pdo->prepare('UPDATE product_list SET quantity = ? WHERE id = ? and list_id = (select id from orders where kunden_id = ? and ordered = 0 and sent = 0)');
+            $stmt = $pdo->prepare('UPDATE user1_list SET quantity = ? WHERE id = ? and list_id = (select id from orders where kunden_id = ? and ordered = 0 and sent = 0)');
             $stmt->bindValue(1, $quantity, PDO::PARAM_INT);
-            $stmt->bindValue(2, $_POST['listid'], PDO::PARAM_INT);
+            $stmt->bindValue(2, $_POST['userid'], PDO::PARAM_INT);
             $stmt->bindValue(3, $user['id'], PDO::PARAM_INT);
             $stmt->execute();
             header('Location: cart.php');
@@ -131,83 +131,71 @@ if(isset($_POST['action'])) {
     }
 }
 
-// SELECT * ,(SELECT img From product_images WHERE product_images.product_id=products.id ORDER BY id LIMIT 1) as image FROM products_types, products where products.product_type_id = products_types.id and products_types.type = 'Test' ORDER BY products.name DESC;
-// Select products ordered by the date added
-$stmt = $pdo->prepare('SELECT *, (SELECT img From product_images WHERE product_images.product_id=products.id ORDER BY id LIMIT 1) AS image, products.quantity as maxquantity FROM products_types, products, product_list where product_list.product_id = products.id and products.product_type_id = products_types.id and products.id in (SELECT product_id FROM product_list where list_id = (select id from orders where kunden_id = ? and ordered = 0 and sent = 0)) and product_list.list_id = (select id from orders where kunden_id = ? and ordered = 0 and sent = 0)');
-#$stmt = $pdo->prepare('SELECT *,(SELECT quantity From product_list WHERE product_list.product_id in (SELECT product_id FROM product_list where list_id = (select id from orders where kunden_id = ? )) LIMIT 1) AS quantity,(SELECT img From product_images WHERE product_images.product_id=products.id ORDER BY id LIMIT 1) AS image FROM products_types, products where products.product_type_id = products_types.id and products.id in (SELECT product_id FROM product_list where list_id = (select id from orders where kunden_id = ? ))');
-$stmt->bindValue(1, $user['id'], PDO::PARAM_INT);
-$stmt->bindValue(2, $user['id'], PDO::PARAM_INT);
+// SELECT * ,(SELECT img From user1_images WHERE user1_images.user1_id=users.id ORDER BY id LIMIT 1) as image FROM users_types, users where users.user1_type_id = users_types.id and users_types.type = 'Test' ORDER BY users.name DESC;
+// Select users ordered by the date added
+$stmt = $pdo->prepare('SELECT * FROM users ORDER BY id');
 $stmt->execute();
-// Get the total number of products
-$total_products = $stmt->rowCount();
-// Fetch the products from the database and return the result as an Array
-$products = $stmt->fetchAll(PDO::FETCH_ASSOC);
-#print_r($products);
+// Get the total number of users
+$total_users = $stmt->rowCount();
+// Fetch the users from the database and return the result as an Array
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+#print_r($users);
 #$stmt->debugDumpParams();
 require_once("templates/header.php");
-$summprice = 0;
-foreach ($products as $product) {
-    $summprice = $summprice + ($product['price'] * $product['quantity']);
-}
 ?>
 
-<div class="container minheight100 products content-wrapper py-3 px-3">
+<div class="container minheight100 users content-wrapper py-3 px-3">
     <div class="row">
         <div class="py-3 px-3 bg-dark rounded">
-            <h1>Warenkorb</h1>
-            <p><?php print($total_products); ?> Produkt<?php if ($total_products > 1) { print('e'); } ?> im Warenkorb</p>
+            <h1>Benutzerverwaltung</h1>
+            <p><?php print($total_users); ?> Benutzer</p>
             <div class="table-responsive">
                 <table class="table">
                     <thead>
                         <tr>
                             <div class="bg-black rounded">
                                 <th scope="col" class="border-0">
-                                    <div class="p-2 px-3 text-uppercase">Produkt</div>
+                                    <div class="p-2 px-3 text-uppercase">Vorname</div>
                                 </th>
                                 <th scope="col" class="border-0 text-center">
-                                    <div class="p-2 px-3 text-uppercase">Preis</div>
+                                    <div class="p-2 px-3 text-uppercase">Nachname</div>
                                 </th>
                                 <th scope="col" class="border-0 text-center">
-                                    <div class="p-2 px-3 text-uppercase">Menge</div>
+                                    <div class="p-2 px-3 text-uppercase">E-Mail</div>
                                 </th>
                                 <th scope="col" class="border-0">
-                                    <div class="p-2 px-3 text-uppercase"></div>
+                                    <div class="p-2 px-3 text-uppercase">Created</div>
                                 </th>
                             </div>
                         </tr>
                     </thead>
                     <tbody>
-                        <?php $i = 0; foreach ($products as $product): ?>
+                        <?php foreach ($users as $user1): ?>
                             <tr>
-                                <th scope="row" class="border-0">
-                                    <div class="p-2">
-                                        <?php if (empty($product['image'])) {
-                                            print('<img src="images/image-not-found.png" width="150" class="img-fluid rounded shadow-sm" alt="' . $product['name'] . '">');
-                                        } else {
-                                            print('<img src="product_img/' . $product['image'] . '" width="150" class="img-fluid rounded shadow-sm" alt="' . $product['name'] . '">');
-                                        }?>
-                                        <div class="ml-3 d-inline-block align-middle">
-                                            <h5 class="mb-0"> 
-                                                <a href="product.php?id=<?=$product['product_id']?>" class="text-white d-inline-block align-middle"><?=$product['name']?></a>
-                                            </h5>
-                                        </div>
-                                    </div>
-                                </th>
                                 <td class="border-0 align-middle text-center">
-                                    <strong><?=$product['price']?></strong>
+                                    <strong><?=$user1['id']?>&euro;</strong>
                                 </td>
                                 <td class="border-0 align-middle text-center">
-                                    <strong><?=$product['quantity']?></strong>
+                                    <strong><?=$user1['vorname']?></strong>
+                                </td>
+                                <td class="border-0 align-middle text-center">
+                                    <strong><?=$user1['nachname']?></strong>
+                                </td>
+                                <td class="border-0 align-middle text-center">
+                                    <strong><a href="mailto:<?=$user1['email']?>"><?=$user1['email']?></a></strong>
+                                </td>
+                                <td class="border-0 align-middle text-center">
+                                    <strong><?=$user1['created_at']?></strong>
                                 </td>
                                 <td class="border-0 align-middle actions">
                                     <form action="cart.php" method="post" class="row me-2">
                                         <div class="col px-3 input-group">
-                                            <input type="number" value="<?=$product['id']?>" name="listid" style="display: none;" required>
-                                            <input class="form-control" type="number" value="<?=$product['quantity']?>" min="1" max="<?=$product['maxquantity']?>" class="form-control form-control-sm" name="quantity" required>
+                                            <input type="number" value="<?=$user1['id']?>" name="userid" style="display: none;" required>
+                                            <input class="form-control" type="number" value="<?=$user1['quantity']?>" min="1" max="<?=$user1['maxquantity']?>" class="form-control form-control-sm" name="quantity" required>
                                             <button type="submit" name="action" value="mod" class="btn btn-outline-primary">Speichern</button>
                                         </div>
                                         <div class="col-3 px-3">
-                                            <input type="number" value="<?=$product['id']?>" name="listid" style="display: none;" required>
+                                            <input type="number" value="<?=$user1['id']?>" name="userid" style="display: none;" required>
                                             <button type="submit" name="action" value="del" class="btn btn-outline-primary">Löschen</button>
                                         </div>
                                     </form>
@@ -217,7 +205,6 @@ foreach ($products as $product) {
                     </tbody>
                 </table>
             </div>         
-            <strong>Summe: <?=$summprice?></strong>
         </div>
     </div>
 </div> 
