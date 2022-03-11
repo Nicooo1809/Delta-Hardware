9a8056152ad775645f60251e86e3983f104c723c
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-11 08:57:35 +0100

    Update user.php

--- user.php
+++ user.php
@@ -110,16 +110,26 @@ if(isset($_POST['action'])) {
 
             <div>
                     <form action="user.php" method="post">
-                        <label for="inputVorname">Vorname</label>
-                        <input class="form-control" id="inputVorname" name="vorname" type="text" value="<?=$user1[0]['vorname']?>" required>
-                        <label for="inputNachname">Nachname</label>
-                        <input class="form-control" id="inputNachname" name="nachname" type="text" value="<?=$user1[0]['nachname']?>" required>
-                        <label for="inputEmail">E-Mail</label>
-                        <input class="form-control" id="inputEmail" name="email" type="email" value="<?=$user1[0]['email']?>" required>
-                        <label for="inputPasswortNeu">Neues Passwort</label>
-                        <input class="form-control" id="inputPasswortNeu" name="passwortNeu" type="password">
-                        <label for="inputPasswortNeu2">Neues Passwort (wiederholen)</label>
-                        <input class="form-control" id="inputPasswortNeu2" name="passwortNeu2" type="password">
+                        <div class="input-group">
+                            <label class="input-group-text" for="inputVorname">Vorname</label>
+                            <input class="form-control" id="inputVorname" name="vorname" type="text" value="<?=$user1[0]['vorname']?>" required>
+                        </div>
+                        <div class="input-group">
+                            <label class="input-group-text" for="inputNachname">Nachname</label>
+                            <input class="form-control" id="inputNachname" name="nachname" type="text" value="<?=$user1[0]['nachname']?>" required>
+                        </div>
+                        <div class="input-group">    
+                            <label class="input-group-text" for="inputEmail">E-Mail</label>
+                            <input class="form-control" id="inputEmail" name="email" type="email" value="<?=$user1[0]['email']?>" required>
+                        </div>
+                        <div class="input-group">
+                            <label class="input-group-text" for="inputPasswortNeu">Neues Passwort</label>
+                            <input class="form-control" id="inputPasswortNeu" name="passwortNeu" type="password">
+                        </div>
+                        <div class="input-group">
+                            <label class="input-group-text" for="inputPasswortNeu2">Neues Passwort (wiederholen)</label>
+                            <input class="form-control" id="inputPasswortNeu2" name="passwortNeu2" type="password">
+                        </div>
                         <?php if ($user['modifyUserPerms'] == 1) {?>
                         <label for="permissions">Permissions</label>
                             <select class="form-select" id="permissions" name="permissions">
