b9bd2ac8dcf6a2e05305f857bf842e5488cfba24
Pflum <pflum@gmx.net>
2022-03-11 14:41:38 +0100

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,21 +1,16 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Dynamic Menu Demo</title>
-    <link rel="stylesheet" href="demo.css"/>
-  </head>
-  <body>
-    <li class="nav-item dropdown">
     <?php
     // (A) GET MENU ITEMS
     require "menu.php";
+    require_once('templates/header.php');
 
     // (B) SUPPORT FUNCTION TO DRAW AN <A>
     function drawlink ($i) {
       printf("<li><a class='dropdown-item' href='". $i["item_link"] . "'>" . $i["item_text"] . "</a></li>"
       );
     }
-
+    ?>
+    <li class="nav-item dropdown">
+    <?php
     // (C) DRAW MENU ITEMS
     foreach ($items[0] as $id=>$i) {
       // (C1) WITH SUB-ITEMS
@@ -33,9 +28,9 @@
     }
     ?>
     </li>
-  </body>
-</html>
-
+<?php
+require_once("templates/footer.php");
+?>
 
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
