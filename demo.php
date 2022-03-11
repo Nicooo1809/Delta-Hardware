40a3d214cc1a5f32763f97bfa6683ad420f038e8
Pflum <pflum@gmx.net>
2022-03-11 14:37:49 +0100

    Update demo.php

--- demo.php
+++ demo.php
@@ -5,15 +5,14 @@
     <link rel="stylesheet" href="demo.css"/>
   </head>
   <body>
-    <nav class="ddmenu"><?php
+    <li class="nav-item dropdown">
+    <?php
     // (A) GET MENU ITEMS
     require "menu.php";
 
     // (B) SUPPORT FUNCTION TO DRAW AN <A>
     function drawlink ($i) {
-      printf("<a %shref='%s'>%s</a>",
-        $i["item_target"]!="" ? "target='". $i["item_target"] ."' " : "" ,
-        $i["item_link"], $i["item_text"]
+      printf("<li><a class='dropdown-item' href='". $i["item_link"] . "'>" . $i["item_text"] . "</a></li>"
       );
     }
 
@@ -23,15 +22,32 @@
       if (isset($items[$id])) { ?>
       <div class="ddgrp">
         <div><?=$i["item_text"]?></div>
-        <div class="dditems"><?php
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown"><?php
           foreach ($items[$id] as $cid=>$c) { drawlink($c); }
-        ?></div>
+        ?></ul>
       </div>
 
       <?php
       // (C2) SINGLE MENU ITEM
       } else { drawlink($i); }
     }
-    ?></nav>
+    ?>
+    </li>
   </body>
 </html>
+
+
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Audio
+    </a>
+    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <li><a class="dropdown-item " href="#">Headsets</a></li>
+        <li><a class="dropdown-item" href="#">Kopfhörer</a></li>
+        <li><a class="dropdown-item" href="#">Mikrofone</a></li>
+        <li><a class="dropdown-item" href="#">Lautsprecher</a></li>
+        <li><a class="dropdown-item" href="#">Soundbar</a></li>
+        <li><a class="dropdown-item" href="#">Soundkarten</a></li>
+    </ul>
+</li>
+
