f7be2f376b0eaf178f3cd7ebc4fa99bcdcec0f95
Nicooo <niggoapfel@gmail.com>
2022-03-11 12:31:42 +0100

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ require_once("templates/header.php");
     <div class="view bg">
         <div class="mask rgba-black-light align-items-center">
             <div class="container">
-                <div class="row minimum-vh justify-content-center align-items-center">
+                <div class="row minimum-vh justify-content-center align-items-left">
                     <div class="col-md-12 mt-1 white-text text-center">
                         <h1 class="h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5"><strong>Die Neue RTX-Reihe</strong></h1>   
                         <hr class="hr-light my-3">
