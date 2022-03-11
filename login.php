92f03fed48c3ae27c8075d3cedaccb408bbe7be0
Nicooo <niggoapfel@gmail.com>
2022-03-11 10:22:52 +0100

    Update login.php

--- login.php
+++ login.php
@@ -60,12 +60,13 @@ include("templates/header.php");
 							
 							<form action="login.php" method="post">
 								<div class="form-floating mb-3">
-									<input id="inputEmail" type="email" name="email" placeholder="E-Mail" value="<?php echo $email_value; ?>" autofocus class="form-control border-0 shadow-sm px-1 ps-2 text-dark fw-bold" required>
+									<input id="inputEmail" type="email" name="email" placeholder="E-Mail" value="<?php echo $email_value; ?>" autofocus class="form-control border-0 shadow-sm ps-4 text-dark fw-bold" required>
 									<label for="inputEmail" class="text-dark fw-bold">E-Mail</label>
 								</div>
-								<div class="form-group mb-3">
-                                    <input id="inputPassword" type="password" name="passwort" placeholder="Passwort" class="form-control border-0 shadow-sm px-4 text-dark fw-bold" required>
-                                </div>
+								<div class="form-floating mb-3">
+                                    <input id="inputPassword" type="password" name="passwort" placeholder="Passwort" class="form-control border-0 shadow-sm ps-4 text-dark fw-bold" required>
+									<label for="inputPassword" class="text-dark fw-bold">Passwort</label>
+								</div>
 
 								<div class="custom-control custom-checkbox mb-3">
 									<input value="remember-me" id="customCheck1" type="checkbox" name="angemeldet_bleiben" value="1" checked class="custom-control-input">
