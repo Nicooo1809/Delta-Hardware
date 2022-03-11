3133293149dabd9e2912783d90c438c2e0b1c578
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-12 00:18:18 +0100

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,8 @@
 require_once("php/functions.php");
 session_start();
 check_user(FALSE);
+
+# Disable Cache so Dark/Lite mode switch works without hard refresh - may find a better solution later on
 header("Expires: Tue, 01 Jan 2000 00:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
