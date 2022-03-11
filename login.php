06ad270c03c639fd8bbf892f72e4424d4e72a1bc
Nicooo <niggoapfel@gmail.com>
2022-03-11 10:03:51 +0100

    Update login.php

--- login.php
+++ login.php
@@ -60,7 +60,7 @@ include("templates/header.php");
 							
 							<form action="login.php" method="post" class="form-floating">
 								<div class="form-group mb-3">
-									<input id="inputEmail" type="email" name="email" placeholder="E-Mail" value="<?php echo $email_value; ?>" autofocus class="form-control border-0 shadow-sm px-4 text-dark fw-bold" required>
+									<input id="floatingInput" type="email" name="email" placeholder="E-Mail" value="<?php echo $email_value; ?>" autofocus class="form-control border-0 shadow-sm px-4 text-dark fw-bold" required>
 								</div>
 								<div class="form-group mb-3">
                                     <input id="inputPassword" type="password" name="passwort" placeholder="Passwort" class="form-control border-0 shadow-sm px-4 text-dark fw-bold" required>
