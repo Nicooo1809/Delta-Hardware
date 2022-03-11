b978e03d4d6db7a5c7cae17e47773439546c52a6
Pflum <pflum@gmx.net>
2022-03-11 22:41:25 +0100

    Update menu.php

--- menu.php
+++ menu.php
@@ -3,7 +3,7 @@ require_once('php/functions.php');
 require_once("templates/header.php");
 $stmt = $pdo->prepare("SELECT * FROM menu_items WHERE parent_id = 0");
 $stmt->execute();
-error_log(pdo_debugStrParams($stmt));
+#error_log(pdo_debugStrParams($stmt));
 $roottypes = $stmt->fetch(PDO::FETCH_ASSOC);
 error_log(print_r($roottypes, true));
 foreach ($roottypes as $roottype) {
@@ -12,7 +12,7 @@ foreach ($roottypes as $roottype) {
   $stmt->execute();
   $subtypes = $stmt->fetch(PDO::FETCH_ASSOC);
   if (isset($subtypes)) {
-  error_log('1');
+  #error_log('1');
   ?>
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
