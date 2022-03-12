d4c92641be458f36146c0c33d07dc3340379ce7c
Pflum <pflum@gmx.net>
2022-03-13 00:27:01 +0100

    Update internal.php

--- internal.php
+++ internal.php
@@ -1,11 +1,11 @@
 <?php
 require_once("php/mysql.php");
 require_once("php/functions.php");
-include("templates/header.php");
 
 //Überprüfe, dass der User eingeloggt ist
 //Der Aufruf von check_user() muss in alle internen Seiten eingebaut sein
 $user = check_user();
+require_once("templates/header.php");
 ?>
 
 <div class="container minheight100 py-3 px-3">
