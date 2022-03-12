1864625326f2ba38fca3cc5929ec19188e3da14a
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-12 22:51:37 +0100

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -31,11 +31,9 @@ if(!isMobile()):
             <div class="col text-center">
                 <a href="/aboutus.php" class="ctext">Über uns</a>
             </div>
-            <div class="col-3">
+            <div class="col-3 text-end ctext">
                 <input class="styleswitcher" type="checkbox" name="switch" id="switch">
                 <label class="styleswitcherlabel" for="switch"></label>
-            </div>
-            <div class="col-3 text-end ctext">
                 Version <?=$vernum?> 
                 <div class="vr mx-1"></div>
                 <?=$verdate?>
