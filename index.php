e48ba193752e259d1540753005991bd9cf5bc264
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-12 21:00:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -27,9 +27,9 @@ require_once("templates/header.php");
             <div class="container">
                 <div class="d-flex flex-row minimum-vh justify-content-start align-items-center">
                     <div class="col-md-12 mt-1 text-white text-start">
-                        <h1 class="h1-reponsive text-white text-uppercase font-weight-bold mb-0 pt-md-5 pt-5"><strong>Die Neue RTX-Reihe</strong></h1>   
+                        <h1 class="h1-reponsive text-white text-uppercase font-weight-bold mb-0 pt-md-5 pt-5"><strong class="text-white">Die Neue RTX-Reihe</strong></h1>   
                         <hr class="hr-light my-3">
-                        <h5 class="text-uppercase mb-4 text-white"><strong>Jetzt kaufen!</strong></h5>
+                        <h5 class="text-uppercase mb-4 text-white"><strong class="text-white">Jetzt kaufen!</strong></h5>
                     </div>
                 </div>
             </div>
