941e44b36d1541d9cef0b82f60a25c05d98393f8
Pflum <pflum@gmx.net>
2022-03-12 22:37:51 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,4 +1,18 @@
 <?php
+require_once("php/functions.php");
+$user = check_user();
+if ($user['showUser'] != 1 or $user['showUserPerms'] != 1) {
+    error('Permission denied!');
+}
+if (isset($get['site'])) {
+    if ($_GET['site'] == 'user') {
+        require_once('user.php');
+    }
+    if ($_GET['site'] == 'permission') {
+        require_once('perms.php');
+    }
+    exit;
+}
 require_once("templates/header.php");
 ?>
 <main>
@@ -8,8 +22,15 @@ require_once("templates/header.php");
                 <div class="card cbg text-center mx-auto" style="width: 75%;">
                     <div class="card-body">
                         <h1 class="card-title mb-2 text-center">Adminbereich</h1>
-                        <a href="user.php">Benutzer</a>
-                        <a href="perms.php">Berechtigungen</a>
+                        <?php
+                            if ($user['showUser'] == 1) {
+                                print('<a href="user.php">Benutzer</a>');
+                            
+                            } 
+                            if ($user['showUserPerms'] == 1) {
+                                print('<a href="perms.php">Berechtigungen</a>');
+                            }
+                        ?>
                     </div>
                 </div>
             </div>
