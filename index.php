8a2cfd5271006b580d7d716cc75ae954fbeb785d
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-12 20:58:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -26,10 +26,10 @@ require_once("templates/header.php");
         <div class="mask rgba-black-light align-items-center">
             <div class="container">
                 <div class="d-flex flex-row minimum-vh justify-content-start align-items-center">
-                    <div class="col-md-12 mt-1 white-text text-start">
-                        <h1 class="h1-reponsive white-text text-uppercase font-weight-bold mb-0 pt-md-5 pt-5"><strong>Die Neue RTX-Reihe</strong></h1>   
+                    <div class="col-md-12 mt-1 text-white text-start">
+                        <h1 class="h1-reponsive text-white text-uppercase font-weight-bold mb-0 pt-md-5 pt-5"><strong>Die Neue RTX-Reihe</strong></h1>   
                         <hr class="hr-light my-3">
-                        <h5 class="text-uppercase mb-4 white-text"><strong>Jetzt kaufen!</strong></h5>
+                        <h5 class="text-uppercase mb-4 text-white"><strong>Jetzt kaufen!</strong></h5>
                     </div>
                 </div>
             </div>
