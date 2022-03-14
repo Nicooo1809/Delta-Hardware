d7e3c4106922873cb3249ae27fdb7a174d8b11c6
Nicooo <niggoapfel@gmail.com>
2022-03-14 08:16:57 +0100

    Update index.php

--- index.php
+++ index.php
@@ -182,16 +182,15 @@ require_once("templates/header.php");
                     </div>
                 </div>
             </div>
-
+            <a class="carousel-control-prev" href="#newproductcarousel" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#newproductcarousel" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
-        <a class="carousel-control-prev" href="#newproductcarousel" role="button" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#newproductcarousel" role="button" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
     </div>
 
 </main>
