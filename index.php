032e7a03c5c233840ffe5c6c5f50b02fb44b18c3
Nicooo <niggoapfel@gmail.com>
2022-03-14 07:48:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -16,58 +16,128 @@ require_once("templates/header.php");
             </div>
         </div>
     </div>
-</main>
-<div id="newproductcarousel" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
 
-                <div class="col-sm">
-                One of three columns
-                </div>
-                <div class="col-sm">
-                One of three columns
-                </div>
-                <div class="col-sm">
-                One of three columns
-                </div>
 
-            </div>
-            <div class="carousel-item">
 
-                <div class="col-sm">
-                One of three columns
-                </div>
-                <div class="col-sm">
-                One of three columns
-                </div>
-                <div class="col-sm">
-                One of three columns
-                </div>
 
-            </div>
-            <div class="carousel-item">
 
-                <div class="col-sm">
-                One of three columns
-                </div>
-                <div class="col-sm">
-                One of three columns
-                </div>
-                <div class="col-sm">
-                One of three columns
+<div id="multi-item-card-carousel" class="carousel slide carousel-multi-item" data-ride="carousel">
+
+    <div class="controls-top">
+        <a class="btn-floating" href="#multi-item-card-carousel" data-slide="prev"><i class="fa fa-chevron-left"></i></a>
+        <a class="btn-floating" href="#multi-item-card-carousel" data-slide="next"><i class="fa fa-chevron-right"></i></a>
+    </div>
+
+
+    <div class="carousel-inner" role="listbox">
+
+        <div class="carousel-item active">
+
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title">Test</h4>
+                            <p class="card-text">Test 0iqefth23 2984t2ßqf fewfg3reezhw</p>
+                            <a class="btn btn-primary">Mehr erfahren</a>
+                        </div>
+                    </div>
                 </div>
 
+                <div class="col-md-4 clearfix d-none d-md-block">
+                    <div class="card mb-2">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title">Test</h4>
+                            <p class="card-text">Test 39e2ut8 g83wuiefhj9823ifj 2039ifjk902wß 42t293jfug8 2hg9324uihgn329urth1329iu hjfgiuhn32 9gu2h3gu98</p>
+                            <a class="btn btn-primary">Mehr erfahren</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-        <a class="carousel-control-prev" href="#newproductcarousel" role="button" data-slide="prev">
+
+
+        <div class="carousel-item">
+
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title">Test</h4>
+                            <p class="card-text">Test</p>
+                            <a class="btn btn-primary">Mehr erfahren</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 clearfix d-none d-md-block">
+                    <div class="card mb-2">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title">Test</h4>
+                            <p class="card-text">Test</p>
+                            <a class="btn btn-primary">Mehr erfahren</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="carousel-item">
+
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card mb-2">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title">Test</h4>
+                            <p class="card-text">Test</p>
+                            <a class="btn btn-primary">Mehr erfahren</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4 clearfix d-none d-md-block">
+                    <div class="card mb-2">
+                        <img class="card-img-top" src="https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(18).jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <h4 class="card-title">Test</h4>
+                            <p class="card-text">Test</p>
+                            <a class="btn btn-primary">Mehr erfahren</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+    <a class="carousel-control-prev" href="#multi-item-card-carousel" role="button" data-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#newproductcarousel" role="button" data-slide="next">
+        <a class="carousel-control-next" href="#multi-item-card-carousel" role="button" data-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
-    </div>
+</div>
+
+
+
+
+
+
+
+
+
+</main>
+
 
 
 
