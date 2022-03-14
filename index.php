410f606dfd77a2ffdcf3c1b308d3fe41fe413788
Nicooo <niggoapfel@gmail.com>
2022-03-14 10:13:29 +0100

    index

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ require_once("templates/header.php");
             <div class="container">
                 <div class="d-flex flex-row minimum-vh justify-content-start align-items-center">
                     <div class="col-md-12 mt-1 text-white text-start">
-                        <h1 class="h1-reponsive text-uppercase fw-bold mb-0 pt-md-5 pt-5 text-success">GEFORCE RTX 30-SERIE</h1>
+                        <h1 class="h1-reponsive text-uppercase fw-bold mb-0 pt-md-5 pt-5 index-rtx-text">GEFORCE RTX 30-SERIE</h1>
                         <h3 class="h3-responsive text-white">DIE ULTIMATIVE WAHL</h3>
                         <hr class="hr-light my-3">
                         <h5 class="text-uppercase mb-4 text-white me-5">
