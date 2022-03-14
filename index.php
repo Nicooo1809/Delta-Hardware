da3c47eeee11152a7d0bf96325494702320838ab
Nicooo <niggoapfel@gmail.com>
2022-03-14 09:59:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,10 +9,10 @@ require_once("templates/header.php");
             <div class="container">
                 <div class="d-flex flex-row minimum-vh justify-content-start align-items-center">
                     <div class="col-md-12 mt-1 text-white text-start">
-                        <h1 class="h1-reponsive text-white text-uppercase fw-bold mb-0 pt-md-5 pt-5">GEFORCE RTX 30-SERIE</h1>
+                        <h1 class="h1-reponsive text-uppercase fw-bold mb-0 pt-md-5 pt-5 text-success">GEFORCE RTX 30-SERIE</h1>
                         <h3 class="h3-responsive text-white">DIE ULTIMATIVE WAHL</h3>
                         <hr class="hr-light my-3">
-                        <h5 class="text-uppercase mb-4 text-white">
+                        <h5 class="text-uppercase mb-4 text-white me-5">
                             Die Grafikprozessoren der GeForce RTX™ 30-Serie liefern die ultimative Leistung für Gamer und Kreative. 
                             Mit der Leistung von Ampere, der RTX-Architektur der 2. Generation von NVIDIA, 
                             und mit neuen RT- sowie Tensor-Recheneinheiten, Streaming-Multiprozessoren für die realistischste 
