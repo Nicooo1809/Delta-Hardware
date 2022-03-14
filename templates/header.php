add892c9bd921b2baab97fce3bcdab79f2e08ae2
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-14 08:24:29 +0100

    .

--- templates/header.php
+++ templates/header.php
@@ -13,7 +13,7 @@ check_user(FALSE);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/css/styles.css?v=<?php print(date("Y.m.d.H.i.s")); ?>">
     <!--link rel="stylesheet"  href="/css/<?php print(check_style());?>.css?v=<?php print(date("Y.m.d.H.i.s")); ?>"-->
     <link rel="icon" type="image/png" href="favicon.png" sizes="1024x1024" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
