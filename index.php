936afc1e30fbf7bd324d3a12a576ad2c6d5c8246
Nicooo <niggoapfel@gmail.com>
2022-03-14 08:13:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -19,185 +19,180 @@ require_once("templates/header.php");
 
 
     <div class="container my-5">
-        <div id="newproductcarousel"class="carousel slide carousel-dark text-center" data-bs-ride="carousel">
+        <div id="newproductcarousel"class="carousel slide text-center" data-bs-ride="carousel">
             <div class="d-flex justify-content-center mb-4">
+
                 <button class="carousel-control-prev position-relative" type="button" data-bs-target="#newproductcarousel" data-bs-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="visually-hidden">Previous</span>
                 </button>
-            <button class="carousel-control-next position-relative" type="button" data-bs-target="#newproductcarousel" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+                <button class="carousel-control-next position-relative" type="button" data-bs-target="#newproductcarousel" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+
+            </div>
+
+            <div class="carousel-inner py-4">
+                <div class="carousel-item active">
+                    <div class="container">
+                        <div class="row">
+
+                            <div class="col-lg-4">
+                                <div class="card">
+                                    <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">
+                                            Some quick example text to build on the card title and
+                                            make up the bulk of the card's content.
+                                        </p>
+                                        <a href="#!" class="btn btn-primary">Mehr erfahren</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 d-none d-lg-block">
+                                <div class="card">
+                                    <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">
+                                            Some quick example text to build on the card title and
+                                            make up the bulk of the card's content.
+                                        </p>
+                                        <a href="#!" class="btn btn-primary">Mehr erfahren</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 d-none d-lg-block">
+                                <div class="card">
+                                    <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">
+                                            Some quick example text to build on the card title and
+                                            make up the bulk of the card's content.
+                                        </p>
+                                        <a href="#!" class="btn btn-primary">Mehr erfahren</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="carousel-item">
+                    <div class="container">
+                        <div class="row">
+
+                            <div class="col-lg-4">
+                                <div class="card">
+                                    <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">
+                                            Some quick example text to build on the card title and
+                                            make up the bulk of the card's content.
+                                        </p>
+                                        <a href="#!" class="btn btn-primary">Mehr erfahren</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 d-none d-lg-block">
+                                <div class="card">
+                                    <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">
+                                            Some quick example text to build on the card title and
+                                            make up the bulk of the card's content.
+                                        </p>
+                                        <a href="#!" class="btn btn-primary">Mehr erfahren</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 d-none d-lg-block">
+                                <div class="card">
+                                    <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">
+                                            Some quick example text to build on the card title and
+                                            make up the bulk of the card's content.
+                                        </p>
+                                        <a href="#!" class="btn btn-primary">Mehr erfahren</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="carousel-item">
+                    <div class="container">
+                        <div class="row">
+
+                            <div class="col-lg-4">
+                                <div class="card">
+                                    <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">
+                                            Some quick example text to build on the card title and
+                                            make up the bulk of the card's content.
+                                        </p>
+                                        <a href="#!" class="btn btn-primary">Mehr erfahren</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 d-none d-lg-block">
+                                <div class="card">
+                                    <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">
+                                            Some quick example text to build on the card title and
+                                            make up the bulk of the card's content.
+                                        </p>
+                                        <a href="#!" class="btn btn-primary">Mehr erfahren</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-4 d-none d-lg-block">
+                                <div class="card">
+                                    <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">
+                                            Some quick example text to build on the card title and
+                                            make up the bulk of the card's content.
+                                        </p>
+                                        <a href="#!" class="btn btn-primary">Mehr erfahren</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
-
-        <div class="carousel-inner py-4">
-            <div class="carousel-item active">
-                <div class="container">
-                    <div class="row">
-
-                        <div class="col-lg-4">
-                            <div class="card">
-                                <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">
-                                        Some quick example text to build on the card title and
-                                        make up the bulk of the card's content.
-                                    </p>
-                                    <a href="#!" class="btn btn-primary">Mehr erfahren</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-4 d-none d-lg-block">
-                            <div class="card">
-                                <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">
-                                        Some quick example text to build on the card title and
-                                        make up the bulk of the card's content.
-                                    </p>
-                                    <a href="#!" class="btn btn-primary">Mehr erfahren</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-4 d-none d-lg-block">
-                            <div class="card">
-                                <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">
-                                        Some quick example text to build on the card title and
-                                        make up the bulk of the card's content.
-                                    </p>
-                                    <a href="#!" class="btn btn-primary">Mehr erfahren</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="carousel-item">
-                <div class="container">
-                    <div class="row">
-
-                        <div class="col-lg-4">
-                            <div class="card">
-                                <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">
-                                        Some quick example text to build on the card title and
-                                        make up the bulk of the card's content.
-                                    </p>
-                                    <a href="#!" class="btn btn-primary">Mehr erfahren</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-4 d-none d-lg-block">
-                            <div class="card">
-                                <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">
-                                        Some quick example text to build on the card title and
-                                        make up the bulk of the card's content.
-                                    </p>
-                                    <a href="#!" class="btn btn-primary">Mehr erfahren</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-4 d-none d-lg-block">
-                            <div class="card">
-                                <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">
-                                        Some quick example text to build on the card title and
-                                        make up the bulk of the card's content.
-                                    </p>
-                                    <a href="#!" class="btn btn-primary">Mehr erfahren</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="carousel-item">
-                <div class="container">
-                    <div class="row">
-
-                        <div class="col-lg-4">
-                            <div class="card">
-                                <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">
-                                        Some quick example text to build on the card title and
-                                        make up the bulk of the card's content.
-                                    </p>
-                                    <a href="#!" class="btn btn-primary">Mehr erfahren</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-4 d-none d-lg-block">
-                            <div class="card">
-                                <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">
-                                        Some quick example text to build on the card title and
-                                        make up the bulk of the card's content.
-                                    </p>
-                                    <a href="#!" class="btn btn-primary">Mehr erfahren</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-4 d-none d-lg-block">
-                            <div class="card">
-                                <img src="https://mdbootstrap.com/img/new/standard/nature/181.jpg" class="card-img-top" alt="...">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">
-                                        Some quick example text to build on the card title and
-                                        make up the bulk of the card's content.
-                                    </p>
-                                    <a href="#!" class="btn btn-primary">Mehr erfahren</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <a class="carousel-control-prev" href="#multi-item-card-carousel" role="button" data-slide="prev">
+        <a class="carousel-control-prev" href="#newproductcarousel" role="button" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="carousel-control-next" href="#multi-item-card-carousel" role="button" data-slide="next">
+        <a class="carousel-control-next" href="#newproductcarousel" role="button" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
     </div>
-
-
-
-
-
-
-
-
 
 </main>
 
