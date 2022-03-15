7cdfa2771a38bd772ebb012ee2c9a3728ae95f9e
Pflum <pflum@gmx.net>
2022-03-15 17:04:28 +0100

    .

--- login.php
+++ login.php
@@ -28,7 +28,7 @@ if(isset($_POST['email']) && isset($_POST['passwort'])) {
 			#error_log(pdo_debugStrParams($insert));
 		}
 
-		header("location: internal.php");
+		header("location: test.php");
 		exit;
 	} else {
 		$error_msg =  "E-Mail oder Passwort war ungültig<br><br>";
