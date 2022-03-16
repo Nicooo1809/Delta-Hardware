ab537393cc7d58c7ede18c33e521dd331c168de3
Pflum <pflum@gmx.net>
2022-03-16 21:39:18 +0100

    .

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,6 @@ require_once("php/functions.php");
 session_start();
 #error_log(print_r($_SESSION['userid'],true));
 #error_log($user1);
-global $user1;
 $user1 = check_user(FALSE);
 
 #error_log($_SESSION['userid']);
