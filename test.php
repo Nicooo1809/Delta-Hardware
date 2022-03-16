82e29dc8c91202fee0d39ae86c6b540d0f178679
Pflum <pflum@gmx.net>
2022-03-16 18:11:24 +0100

    .

--- test.php
+++ test.php
@@ -3,5 +3,11 @@ require_once("php/functions.php");
 $user = check_user();
 #print_r(check_user(FALSE));
 require_once("templates/header.php");
+?>
+tztrztrzrtzrtzrtzrtztziurhrwetughgfuxbngzubvdfzibgzred
+dfgfdghgf
+hfghfg
+hfghfgh
+<?php
 require_once("templates/footer.php");
 ?>
