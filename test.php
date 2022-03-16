0867ca5498d30c2df95faf02cab25e2c1a134b7e
Pflum <pflum@gmx.net>
2022-03-16 18:22:33 +0100

    .

--- test.php
+++ test.php
@@ -4,10 +4,7 @@ $user = check_user();
 require_once("templates/header.php");
 print_r($user);
 ?>
-tztrztrzrtzrtzrtzrtztziurhrwetughgfuxbngzubvdfzibgzred
-dfgfdghgf
-hfghfg
-hfghfgh
+
 <?php
 require_once("templates/footer.php");
 ?>
