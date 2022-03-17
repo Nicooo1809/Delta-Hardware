aeed3a84f6bef4e99a97b791a7d5f8c69eacd4cf
Pflum <pflum@gmx.net>
2022-03-17 16:38:45 +0100

    .

--- test.php
+++ test.php
@@ -1,9 +1,8 @@
 <?php
 require_once("php/functions.php");
-#$user = check_user();
 $user = require_once("templates/header.php");
-#global $user1;
-print_r($user1);
+if (!isset($user['id']))
+print_r($user);
 ?>
 
 <?php
