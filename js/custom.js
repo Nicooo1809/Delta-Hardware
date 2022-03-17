6ade3ed414e1282bf877af278da9a3ae9c24607e
Pflum <pflum@gmx.net>
2022-03-17 07:27:46 +0100

    .

--- js/custom.js
+++ js/custom.js
@@ -15,22 +15,30 @@ function setStyle() {
   if (getCookie("style") == "dark") {
     document.getElementById('style_switch').checked = true;
     setCookie("style", "dark", 365);
+    document.querySelectorAll("link[href='/css/dark.css']")[0].disabled = false;
+    document.querySelectorAll("link[href='/css/light.css']")[0].disabled = true;
     //document.getElementById("theme_css").remove()
+    /*
     var fileref = document.createElement("link");
     fileref.setAttribute("rel", "stylesheet");
     fileref.setAttribute("type", "text/css");
     fileref.setAttribute("href", "/css/dark.css");
     //fileref.setAttribute("id", "theme_css");
     document.getElementsByTagName("head")[0].appendChild(fileref);
+    */
   } else {
     setCookie("style", "light", 365);
+    document.querySelectorAll("link[href='/css/dark.css']")[0].disabled = true;
+    document.querySelectorAll("link[href='/css/light.css']")[0].disabled = false;
     //document.getElementById("theme_css").remove()
+    /*
     var fileref = document.createElement("link");
     fileref.setAttribute("rel", "stylesheet");
     fileref.setAttribute("type", "text/css");
     fileref.setAttribute("href", "/css/light.css");
     //fileref.setAttribute("id", "theme_css");
     document.getElementsByTagName("head")[0].appendChild(fileref);
+    */
   }
 }
 
