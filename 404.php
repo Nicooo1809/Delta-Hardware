09d2129a0a88449156e6cf0ee658bea606f308e0
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-17 23:00:31 +0100

    Update 404.php

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 require_once("templates/header.php");
 ?>
 <div class="container-fluid minheight100">
-    <div class="justify-content-end align-items-center">
+    <div class="position-absolute top-50 start-50 translate-middle">
         <img src="media/404.gif" class="img-fluid rounded">
     </div>
 </div>
