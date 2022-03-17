cf6e4ac8423992f62d3fc79561d83fdb99057bdd
Pflum <pflum@gmx.net>
2022-03-17 16:42:21 +0100

    .

--- test.php
+++ test.php
@@ -1,7 +1,10 @@
 <?php
 require_once("php/functions.php");
 $user = require_once("templates/header.php");
-if (!isset($user['id']))
+if (!isset($user['id'])) {
+    require_once("login.php");
+    exit;
+}
 print_r($user);
 ?>
 
