c2b5a5972b899ffd37fd2e480c59a5b6fe21311b
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-17 22:50:44 +0100

    .

--- 404.php
+++ 404.php
@@ -2,10 +2,10 @@
 require_once("templates/header.php");
 ?>
 <div class="container-fluid minheight100">
-
-
-
-
+    <div>
+        <img src="media/404.gif" class="img-fluid rounded">
+    </div>
+</div>
 <?php
 include_once("templates/footer.php")
 ?>
