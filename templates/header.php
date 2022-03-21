4cf1091f2b37335c0a8e2370d0e20e5b577a911c
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-21 12:53:46 +0100

    .

--- templates/header.php
+++ templates/header.php
@@ -21,7 +21,7 @@ $user1 = check_user(FALSE);
     <link rel="stylesheet" href="/css/styles.css?v=<?php print(date("Y.m.d.H.i.s")); ?>">
     <link rel="stylesheet"  href="/css/dark.css" disabled>
     <link rel="stylesheet"  href="/css/light.css" disabled>
-    <link rel="icon" type="image/png" href="favicon_<?php print(check_style())?>.png" sizes="1024x1024" />
+    <link rel="icon" type="image/png" href="favicon_dark.png" sizes="1024x1024" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/3386a0b16e.js" crossorigin="anonymous"></script>
