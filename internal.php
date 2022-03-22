7dda36989befb1cebc320391766c7f221e9e0796
Pflum <pflum@gmx.net>
2022-03-22 11:29:29 +0100

    .

--- internal.php
+++ internal.php
@@ -19,7 +19,7 @@ if (!isset($user['id'])) {
 					Hallo <?=$user['vorname']?>,<br>
 					Herzlich Willkommen im internen Bereich!<br>
 				</span>
-				<button type="button" href="logout.php" class="btn btn-outline-primary my-3">Abmelden</button>
+				<a href="logout.php"><button type="button" class="btn btn-outline-primary my-3">Abmelden</button></a>
 			</div>
 		</div>
 		<?php if ($user['showUser'] == 1 or $user['showUserPerms'] == 1) { ?>
@@ -29,11 +29,11 @@ if (!isset($user['id'])) {
 				<div class="card-text">
 					<?php
 						if ($user['showUser'] == 1) {
-							print('<a href="/user.php"><button class="btn btn-outline-primary mx-2" type="submit">Benutzer</button></a>');
+							print('<a href="user.php"><button class="btn btn-outline-primary mx-2" type="button">Benutzer</button></a>');
 						
 						} 
 						if ($user['showUserPerms'] == 1) {
-							print('<a href="/perms.php"><button class="btn btn-outline-primary mx-2" type="submit">Berechtigungen</button></a>');
+							print('<a href="perms.php"><button class="btn btn-outline-primary mx-2" type="button">Berechtigungen</button></a>');
 						}
 					?>
 				</div>
