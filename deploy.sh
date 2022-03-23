fcf85215a63ddc72694d137594b314a5124d650a
Pflum <pflum@gmx.net>
2022-03-23 11:37:30 +0100

    .

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 echo 'DROP DATABASE IF EXISTS shop;' | mysql -uroot -p000000
-echo 'CREATE DATABASE IF NOT EXISTS shop;' | mysql -uroot -p000000
-mysql -uroot -p000000 < `pwd`/hidden/database.sql
+echo 'CREATE DATABASE shop;' | mysql -uroot -p000000
+echo "CREATE USER IF NOT EXISTS 'shopuser'@'localhost' IDENTIFIED BY '';" | mysql -uroot -p000000
+mysql -uroot -p000000 shop < `pwd`/hidden/database.sql
+echo "GRANT ALL PRIVILEGES ON shop.* TO 'shopuser'@'localhost';" | mysql -uroot -p000000
+echo 'FLUSH PRIVILEGES;' | mysql -uroot -p000000
 
 php -S localhost:8080
