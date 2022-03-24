f2c25441a0a3524eeca0ba07d62d388e15a57f21
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-03-24 21:44:29 +0100

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -19,7 +19,7 @@
 <?php
 $vernum = "0.0.7";
 # Like this, cause we want the Date the current Version was developed, not the current dates
-$verdate ="21.03.2022";
+$verdate ="24.03.2022";
 #$verdate = date("d.m.Y");
 if(!isMobile()):
 ?>
