1c061d02831a59d4b9831be7e715a544040c1b8f
Pflum <pflum@gmx.net>
2022-03-28 08:04:55 +0200

    .

--- test.php
+++ test.php
@@ -8,74 +8,44 @@ $user = require_once("templates/header.php");
 
 
 
-
-if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
-    // Allow certain file formats
-    $fileCount = count($_FILES['file']['name']);
-    for($i = 0; $i < $fileCount; $i++){
-        $fileName = basename($_FILES["file"]["name"][$i]);
-        $targetFilePath = "uploads/" . $fileName;
-
-
-        $allowTypes = array('jpg','png','jpeg','gif');
-        if(in_array(pathinfo($targetFilePath,PATHINFO_EXTENSION), $allowTypes)){
-            // Upload file to server
-            if(move_uploaded_file($_FILES["file"]["tmp_name"][$i], $targetFilePath)){
-                // Insert image file name into database
-                $stmt = $pdo->prepare("INSERT into product_images (img, product_id) VALUES ( ? , ? )");
-                $stmt->bindValue(1, $fileName);
-                $stmt->bindValue(2, 1, PDO::PARAM_INT);
-                $stmt->execute();
-                if($stmt){
-                    $statusMsg = "The file ".$fileName. " has been uploaded successfully.";
+$allowTypes = array('jpg','png','jpeg','gif');
+if(isset($_POST["submit"])){
+    if(!empty($_FILES["file"]["name"][0])){
+        // Allow certain file formats
+        $fileCount = count($_FILES['file']['name']);
+        for($i = 0; $i < $fileCount; $i++){
+            $fileName = basename($_FILES["file"]["name"][$i]);
+            $targetFilePath = "uploads/" . $fileName;
+            if(in_array(pathinfo($targetFilePath,PATHINFO_EXTENSION), $allowTypes)){
+                // Upload file to server
+                if(move_uploaded_file($_FILES["file"]["tmp_name"][$i], $targetFilePath)){
+                    // Insert image file name into database
+                    $stmt = $pdo->prepare("INSERT into product_images (img, product_id) VALUES ( ? , ? )");
+                    $stmt->bindValue(1, $fileName);
+                    $stmt->bindValue(2, 1, PDO::PARAM_INT);
+                    $stmt->execute();
+                    if(!$stmt){
+                        $statusMsg = "File upload failed, please try again.";
+                    } 
                 }else{
-                    $statusMsg = "File upload failed, please try again.";
-                } 
+                    $statusMsg = "Sorry, there was an error uploading your file.";
+                }
             }else{
-                $statusMsg = "Sorry, there was an error uploading your file.";
+                $statusMsg = 'Sorry, only JPG, JPEG, PNG & GIF files are allowed to upload.';
             }
-        }else{
-            $statusMsg = 'Sorry, only JPG, JPEG, PNG & GIF files are allowed to upload.';
         }
+    }else{
+        $statusMsg = 'Please select a file to upload.';
     }
-
-
-
-}else{
-    $statusMsg = 'Please select a file to upload.';
 }
-
 // Display status message
 echo $statusMsg;
-
-
-
-/*
-
-if(isset($_POST['submit'])){
- 
-    // Count total files
-    $fileCount = count($_FILES['file']['name']);
-
-    // Iterate through the files
-    for($i = 0; $i < $fileCount; $i++){
-
-         $file = $_FILES['file']['name'][$i];
-
-         // Upload file to $path
-         move_uploaded_file($_FILES['file']['tmp_name'][$i], $file);
-
-    }
-} 
-
-*/
-
 
 ?>
 
 <form action="test.php" method="post" enctype="multipart/form-data">
     Select Image File to Upload:
-    <input type="file" name="file" multiple>
+    <input type="file" name="file[]" multiple>
     <input type="submit" name="submit" value="Upload">
 </form>
 
