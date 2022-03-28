fafd5f4924c2ee653fa8ebfe2f66c07ad4c68d00
Pflum <pflum@gmx.net>
2022-03-28 21:20:31 +0200

    .

--- produc.php
+++ produc.php
@@ -18,10 +18,7 @@ if(isset($_POST['action'])) {
         $stmt->bindValue(1, $_POST['productid'], PDO::PARAM_INT);
         $stmt->execute();
         $product = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
-        $stmt = $pdo->prepare('SELECT * FROM permission_group');
-        $stmt->execute();
-        $permissions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
         if(isset($_POST['vorname']) and isset($_POST['nachname']) and isset($_POST['email']) and isset($_POST['passwortNeu']) and isset($_POST['passwortNeu2']) and !empty($_POST['vorname']) and !empty($_POST['nachname']) and !empty($_POST['email'])) {
             
             $stmt = $pdo->prepare("UPDATE products SET email = ?, vorname = ?, nachname = ?, updated_at = now() WHERE products.id = ?");
@@ -31,8 +28,8 @@ if(isset($_POST['action'])) {
             $stmt->bindValue(4, $_POST['productid'], PDO::PARAM_INT);
             $stmt->execute();
             #error_log(pdo_debugStrParams($stmt));
-            echo("<script>location.href='product.php'</script>");
-            #header("location: product.php");
+            echo("<script>location.href='produc.php'</script>");
+            #header("location: produc.php");
             exit;
         } else {
         require_once("templates/header.php");
@@ -40,7 +37,7 @@ if(isset($_POST['action'])) {
         <div class="minheight100 px-3 py-3">
             <h1>Einstellungen</h1>
             <div>
-                <form action="product.php" method="post">
+                <form action="produc.php" method="post">
                     <div class="input-group py-2">
                         <span class="input-group-text" for="inputVorname">Vorname</span>
                         <input class="form-control" id="inputVorname" name="vorname" type="text" value="<?=$product[0]['vorname']?>" required>
@@ -73,13 +70,13 @@ if(isset($_POST['action'])) {
         } 
     }
     if ($_POST['action'] == 'cancel') {
-        echo("<script>location.href='product.php'</script>");
-        #header("location: product.php");
+        echo("<script>location.href='produc.php'</script>");
+        #header("location: produc.php");
         exit;
     }
 }
 
-$stmt = $pdo->prepare('SELECT * FROM permission_group, products where products.permission_group = permission_group.id ORDER BY products.id');
+$stmt = $pdo->prepare('SELECT * FROM products_types, products where products.product_type_id = products_types.id ORDER BY products.id;');
 $stmt->execute();
 $total_products = $stmt->rowCount();
 $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -90,8 +87,13 @@ $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <div class="container minheight100 users content-wrapper py-3 px-3">
     <div class="row">
         <div class="py-3 px-3 cbg ctext rounded">
-            <h1>Benutzerverwaltung</h1>
-            <a href="register.php">Benutzer hinzufügen</a>
+            <h1>Produktverwaltung</h1>
+            <form action="produc.php" method="post">
+                <div>
+                    <button type="submit" name="action" value="add" class="btn btn-outline-primary">Editieren</button>
+                </div>
+            </form>
+            <a href="register.php">Produkt hinzufügen</a>
             <p><?php print($total_products); ?> Benutzer</p>
             <div class="table-responsive">
                 <table class="table align-middle table-borderless table-hover">
@@ -102,21 +104,29 @@ $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
                                     <div class="p-2 px-3 text-uppercase ctext">#</div>
                                 </th>
                                 <th scope="col" class="border-0 text-center">
-                                    <div class="p-2 px-3 text-uppercase ctext">Vorname</div>
+                                    <div class="p-2 px-3 text-uppercase ctext">Name</div>
                                 </th>
                                 <th scope="col" class="border-0 text-center">
-                                    <div class="p-2 px-3 text-uppercase ctext">Nachname</div>
+                                    <div class="p-2 px-3 text-uppercase ctext">Producttype</div>
                                 </th>
                                 <th scope="col" class="border-0 text-center">
-                                    <div class="p-2 px-3 text-uppercase ctext">E-Mail</div>
+                                    <div class="p-2 px-3 text-uppercase ctext">RRP</div>
                                 </th>
                                 <th scope="col" class="border-0 text-center">
-                                    <div class="p-2 px-3 text-uppercase ctext">Rechte</div>
+                                    <div class="p-2 px-3 text-uppercase ctext">Preis</div>
                                 </th>
                                 <th scope="col" class="border-0">
-                                    <div class="p-2 px-3 text-uppercase ctext">Erstellt</div>
+                                    <div class="p-2 px-3 text-uppercase ctext">Quantity</div>
                                 </th>
-                                <th scope="col" class="border-0" style="width: 15%"></th>
+                                <th scope="col" class="border-0">
+                                    <div class="p-2 px-3 text-uppercase ctext">Created</div>
+                                </th>
+                                <th scope="col" class="border-0">
+                                    <div class="p-2 px-3 text-uppercase ctext">Visible</div>
+                                </th>
+                                <?php if ($user['modifyProduct'] == 1) {
+                                    print('<th scope="col" class="border-0"></th>');
+                                } ?>
                             </div>
                         </tr>
                     </thead>
@@ -127,46 +137,33 @@ $products = $stmt->fetchAll(PDO::FETCH_ASSOC);
                                     <strong><?=$product['id']?></strong>
                                 </td>
                                 <td class="border-0 text-center">
-                                    <strong><?=$product['vorname']?></strong>
-                                </td>
-                                <td class="border-0 text-center">
-                                    <strong><?=$product['nachname']?></strong>
-                                </td>
-                                <td class="border-0 bl-400 text-center"">
-                                    <strong><a href="mailto:<?=$product['email']?>"><?=$product['email']?></a></strong>
-                                </td>
-                                <td class="border-0 text-center">
                                     <strong><?=$product['name']?></strong>
+                                </td>
+                                <td class="border-0 text-center">
+                                    <strong><?=$product['type']?></strong>
+                                </td>
+                                <td class="border-0 text-center">
+                                    <strong><?=$product['rrp']?></strong>
+                                </td>
+                                <td class="border-0 text-center">
+                                    <strong><?=$product['price']?></strong>
+                                </td>
+                                <td class="border-0 text-center">
+                                    <strong><?=$product['quantity']?></strong>
                                 </td>
                                 <td class="border-0">
                                     <strong><?=$product['created_at']?></strong>
                                 </td>
+                                <td class="border-0">
+                                    <strong><input type="checkbox" class="form-check-input" <?=($product['visible']==1 ? 'checked':'')?> disabled></strong>
+                                </td>
                                 <td class="border-0 actions text-center">
-                                <?php if ($user['modifyProduct'] == 1 or $user['deleteUser'] == 1) {?>
-                                    <form action="product.php" method="post" class="d-grid gap-2 d-md-flex justify-content-md-end">
-                                        <?php if ($user['modifyProduct'] == 1) {?>
-                                        <div class="">
+                                <?php if ($user['modifyProduct'] == 1) {?>
+                                    <form action="produc.php" method="post" class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                        <div>
                                             <input type="number" value="<?=$product['id']?>" name="productid" style="display: none;" required>
                                             <button type="submit" name="action" value="mod" class="btn btn-outline-primary">Editieren</button>
                                         </div>
-                                        <?php }?>
-                                        <?php if ($user['deleteUser'] == 1) {?>
-                                        <div class="">
-                                            <input type="number" value="<?=$product['id']?>" name="productid" style="display: none;" required>
-                                            <button class="btn btn-outline-danger" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas<?=$product['id']?>" aria-controls="offcanvas<?=$product['id']?>">Löschen</button>
-                                                <div class="offcanvas offcanvas-end cbg" data-bs-scroll="true" tabindex="-1" id="offcanvas<?=$product['id']?>" aria-labelledby="offcanvas<?=$product['id']?>Label">
-                                                    <div class="offcanvas-header">
-                                                        <h2 class="offcanvas-title ctext" id="offcanvas<?=$product['id']?>Label">Wirlich Löschen?</h2>
-                                                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                                                    </div>
-                                                    <div class="offcanvas-body">
-                                                        <button class="btn btn-outline-success mx-2" type="submit" name="action" value="deleteconfirm">Ja</button>
-                                                        <button class="btn btn-outline-danger mx-2" type="button" data-bs-dismiss="offcanvas" aria-label="Close">Nein</button>
-                                                    </div>
-                                                </div>
-                                            <!-- <button type="submit" name="action" value="del" class="btn btn-outline-danger">Löschen</button> -->
-                                        </div>
-                                        <?php }?>
                                     </form>
                                 <?php }?>
                                 </td>
