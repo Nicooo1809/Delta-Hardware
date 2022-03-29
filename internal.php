9e7ea9d77f87e87262b6513c5e8e058d240716d8
Pflum <pflum@gmx.net>
2022-03-29 12:14:47 +0200

    .

--- internal.php
+++ internal.php
@@ -25,7 +25,7 @@ if (!isset($user['id'])) {
 				</div>
 			</div>
 		</div>
-		<?php if ($user['showUser'] == 1 or $user['showUserPerms'] == 1) { ?>
+		<?php if ($user['showUser'] == 1 or $user['showUserPerms'] == 1 or $user['showCategories'] == 1 or $user['showProduct'] == 1) { ?>
 		<div class="card cbg ctext my-3 mx-auto">
 			<div class="card-body text-center">
 				<h1 class="card-title">Adminbereich</h1>
@@ -33,7 +33,6 @@ if (!isset($user['id'])) {
 					<?php
 						if ($user['showUser'] == 1) {
 							print('<a href="user.php"><button class="btn btn-outline-primary mx-2 my-2" type="button">Benutzer</button></a>');
-						
 						} 
 						if ($user['showUserPerms'] == 1) {
 							print('<a href="perms.php"><button class="btn btn-outline-primary mx-2 my-2" type="button">Berechtigungen</button></a>');
@@ -41,6 +40,21 @@ if (!isset($user['id'])) {
 						if ($user['showCategories'] == 1) {
 							print('<a href="categories.php"><button class="btn btn-outline-primary mx-2 my-2" type="button">Kategorien</button></a>');
 						}
+						if ($user['showProduct'] == 1) {
+							print('<a href="produc.php"><button class="btn btn-outline-primary mx-2 my-2" type="button">Produkte</button></a>');
+						}
+					?>
+				</div>
+			</div>
+		</div>
+		<?php } ?>
+		<?php if ($user['showOrders'] == 1) { ?>
+		<div class="card cbg ctext my-3 mx-auto">
+			<div class="card-body text-center">
+				<h1 class="card-title">Bestellungen</h1>
+				<div class="card-text">
+					<?php
+						# foreach
 					?>
 				</div>
 			</div>
