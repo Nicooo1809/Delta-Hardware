fc91820626061c48ae0421ffecdb9fb88198cf3a
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-04-07 17:56:01 +0200

    Update order.php

--- admin/order.php
+++ admin/order.php
@@ -47,17 +47,21 @@ foreach ($products as $product) {
     <div class="container minheight100 products content-wrapper py-3 px-3">
         <div class="row">
             <div class="py-3 px-3 cbg ctext rounded">
-                <h1>Bestellung bearbeiten</h1>
-                <p>Bitte folgende<?=($total_products>1 ? ' '.$total_products:'s')?> Produkt<?=($total_products>1 ? 'e':'')?> für den Kunden einpacken und das Packet mit folgendem Addressaufkleber versehen:</p>
-				<p><?=$customer[0]['vorname'].' '.$customer[0]['nachname']?></br>
-				<?=$customer[0]['streetHouseNr']?></br>
-				<?=$customer[0]['city']?></p>
-                <?php if ($user['markOrders'] == 1) { ?>
-                    <form action="?id=<?=$_GET['id']?>" method="post" class="d-flex justify-content-end mb-2">
-                        <button type="submit" name="confirm" value="yes" class="py-2 btn btn-outline-success me-2">Erledigt</button>
-                        <button class="py-2 ms-2 btn btn-outline-danger" type="button" onclick="window.location.href = '/internal.php';">Abbrechen</button>
-                    </form>
-                <?php } ?>
+                <div class="col-6">
+                    <h1>Bestellung bearbeiten</h1>
+                    <p>Bitte folgende<?=($total_products>1 ? ' '.$total_products:'s')?> Produkt<?=($total_products>1 ? 'e':'')?> für den Kunden einpacken und das Packet mit folgendem Addressaufkleber versehen:</p>
+                    <p><?=$customer[0]['vorname'].' '.$customer[0]['nachname']?></br>
+                    <?=$customer[0]['streetHouseNr']?></br>
+                    <?=$customer[0]['city']?></p>
+                </div>
+                <div class="col-6">
+                    <?php if ($user['markOrders'] == 1) { ?>
+                        <form action="?id=<?=$_GET['id']?>" method="post" class="d-flex justify-content-end mb-2">
+                            <button type="submit" name="confirm" value="yes" class="py-2 btn btn-outline-success me-2">Erledigt</button>
+                            <button class="py-2 ms-2 btn btn-outline-danger" type="button" onclick="window.location.href = '/internal.php';">Abbrechen</button>
+                        </form>
+                    <?php } ?>
+                </div>
                 <div class="table-responsive">
                     <table class="table">
                         <thead>
