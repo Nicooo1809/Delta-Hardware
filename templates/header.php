9941e5966ff5f57e3a0b861f4ce48f704e6f06fa
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-04-07 09:56:13 +0200

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -71,7 +71,7 @@ $user1 = check_user(FALSE);
                             <?php if ($subtype['type'] == "line"):?>
                                 <li><hr class="dropdown-divider"></li>
                             <?php else:?>
-                                <li><a class="dropdown-item" href="products.php?type=<?=$subtype['id']?>"><?=$subtype['type']?>  <span class="text-end"><?=$subtype['quantity']?><span></a></li>
+                                <li><a class="dropdown-item stretched-link" href="products.php?type=<?=$subtype['id']?>"><?=$subtype['type']?></a><span class="text-end"><?=$subtype['quantity']?><span></li>
                             <?php endif; ?>
                         <?php
                         }
