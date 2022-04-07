527425ef999458a379965ef11cbf5f5eb7e997b2
Nicooo <niggoapfel@gmail.com>
2022-04-07 09:43:52 +0200

    Update cookies.php

--- cookies.php
+++ cookies.php
@@ -7,15 +7,15 @@ require_once("templates/header.php");
         <div class="col">
             <div class="card cbg ctext mx-auto" style="width: <?php if(!isMobile()) print('75%'); else print('95%');?>;">
                 <div class="card-body">
-                <h4 class="fw-bold ctext">Wir verwenden nur notwendige Cookies aus lokalem Anbau um folgende Funktion bereitzustellen:</h4>
+                <h4 class="fw-bold ctext">Wir verwenden nur notwendige Cookies um folgende Funktion bereitzustellen:</h4>
                     <br>
                     <p class="fs-5 ctext cookie-p-text">- Speichern der PHP-Session</p>
                     <p class="fs-5 ctext cookie-p-text">- Angemeldet bleiben</p>
                     <p class="fs-5 ctext cookie-p-text">- Speichern der Style-Einstellung</p>
                     <p class="fs-5 ctext cookie-p-text mb-1">- Speichern der Cookie-Einstellung</p>
                     <br>
-                    <p class="fw-light fs-6 cookie-p-text ctext">Ihre Cookie-Einstellung wird gespeichert.</p>
-                    <a href="index"><button type="button" class="btn btn-secondary">Zurück zur Startseite</button></a>
+                    <p class="fw-light fs-6 cookie-p-text ctext mb-5">Ihre Cookie-Einstellung wird gespeichert.</p>
+                    <a href="index"><button type="button" class="btn btn-primary">Zurück zur Startseite</button></a>
                 </div>
             </div>
         </div>
