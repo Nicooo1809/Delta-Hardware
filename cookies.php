83074d8fb51f73d030d4419db9a9f440671dd568
Nicooo <niggoapfel@gmail.com>
2022-04-07 09:42:27 +0200

    Update cookies.php

--- cookies.php
+++ cookies.php
@@ -2,18 +2,12 @@
 require_once("templates/header.php");
 ?>
 
-
-<div class="modal fade" id="cookieModal" tabindex="-1" aria-labelledby="cookieModalLabel" aria-hidden="true">
-    <div class="modal-dialog cbg">
-        <div class="modal-content cbg">
-            <div class="modal-header cbg">
-                <h4 class="modal-title ctext fw-bold" id="cookieModalLabel">Mhhh Lecker &#x1F36A;!</h4>
-                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <hr>
-            <div class="modal-body ctext cbg fw-normal">
-                <div class="px-2">
-                    <h4 class="fw-bold ctext">Wir verwenden nur notwendige Cookies aus lokalem Anbau um folgende Funktion bereitzustellen:</h4>
+<div class="container-fluid minheight100 py-4 px-3">
+    <div class="row no-gutter">
+        <div class="col">
+            <div class="card cbg ctext mx-auto" style="width: <?php if(!isMobile()) print('75%'); else print('95%');?>;">
+                <div class="card-body">
+                <h4 class="fw-bold ctext">Wir verwenden nur notwendige Cookies aus lokalem Anbau um folgende Funktion bereitzustellen:</h4>
                     <br>
                     <p class="fs-5 ctext cookie-p-text">- Speichern der PHP-Session</p>
                     <p class="fs-5 ctext cookie-p-text">- Angemeldet bleiben</p>
@@ -21,15 +15,12 @@ require_once("templates/header.php");
                     <p class="fs-5 ctext cookie-p-text mb-1">- Speichern der Cookie-Einstellung</p>
                     <br>
                     <p class="fw-light fs-6 cookie-p-text ctext">Ihre Cookie-Einstellung wird gespeichert.</p>
+                    <a href="index"><button type="button" class="btn btn-secondary">Zurück zur Startseite</button></a>
                 </div>
-            </div>
-            <div class="modal-footer ctext cbg fw-bold">
-                <a href="/"><button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Zurück zur Startseite</button></a>
             </div>
         </div>
     </div>
 </div>
-    
 <?php
 include_once("templates/footer.php")
 ?>
