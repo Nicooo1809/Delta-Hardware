bf1785d1429c0f45d59352b72dc4464b6e9ef0c8
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-04-11 09:49:17 +0200

    Update product.php

--- admin/product.php
+++ admin/product.php
@@ -65,11 +65,15 @@ if(isset($_POST['action'])) {
                 }
             }
         }
-        if(isset($_POST['name']) and isset($_POST['price']) and isset($_POST['rrp']) and isset($_POST['quantity']) and isset($_POST['desc']) and isset($_POST['productid']) and isset($_POST['categorie']) and !empty($_POST['name']) and !empty($_POST['price']) and !empty($_POST['rrp']) and !empty($_POST['desc']) and !empty($_POST['productid']) and !empty($_POST['categorie'])) {
+        if(isset($_POST['name']) and isset($_POST['price']) and isset($_POST['rrp']) and isset($_POST['quantity']) and isset($_POST['desc']) and isset($_POST['productid']) and isset($_POST['categorie']) and !empty($_POST['name']) and !empty($_POST['price']) and !empty($_POST['desc']) and !empty($_POST['productid']) and !empty($_POST['categorie'])) {
+            $rrp = $_POST['rrp'];
+            if(!empty($_POST['rrp'])) {
+                $rrp = str_replace(",", ".", $_POST['rrp']);
+            }
             $stmt = $pdo->prepare("UPDATE products SET name = ?, price = ?, rrp = ?, quantity = ?, `desc` = ?, visible = ?, product_type_id = ?, updated_at = now() WHERE products.id = ?");
             $stmt->bindValue(1, $_POST['name']);
             $stmt->bindValue(2, str_replace(",", ".", $_POST['price']));
-            $stmt->bindValue(3, str_replace(",", ".", $_POST['rrp']));
+            $stmt->bindValue(3, $rrp);
             $stmt->bindValue(4, $_POST['quantity']);
             $stmt->bindValue(5, $_POST['desc']);
             $stmt->bindValue(6, (isset($_POST['visible']) ? "1" : "0"), PDO::PARAM_INT);
@@ -164,11 +168,17 @@ if(isset($_POST['action'])) {
         $stmt->execute();
         $types = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-        if(isset($_POST['name']) and isset($_POST['price']) and isset($_POST['rrp']) and isset($_POST['quantity']) and isset($_POST['desc']) and isset($_POST['categorie']) and !empty($_POST['name']) and !empty($_POST['price']) and !empty($_POST['rrp']) and !empty($_POST['desc']) and !empty($_POST['categorie'])) {
+        
+        
+        if(isset($_POST['name']) and isset($_POST['price']) and isset($_POST['rrp']) and isset($_POST['quantity']) and isset($_POST['desc']) and isset($_POST['categorie']) and !empty($_POST['name']) and !empty($_POST['price']) and !empty($_POST['desc']) and !empty($_POST['categorie'])) {
+            $rrp = $_POST['rrp'];
+            if(!empty($_POST['rrp'])) {
+                $rrp = str_replace(",", ".", $_POST['rrp']);
+            }
             $stmt = $pdo->prepare("INSERT INTO products (name, price, rrp, quantity, `desc`, visible, product_type_id, updated_at, created_at) VALUE (?, ?, ?, ?, ?, ?, ?, now(), now())");
             $stmt->bindValue(1, $_POST['name']);
             $stmt->bindValue(2, str_replace(",", ".", $_POST['price']));
-            $stmt->bindValue(3, str_replace(",", ".", $_POST['rrp']));
+            $stmt->bindValue(3, $rrp);
             $stmt->bindValue(4, $_POST['quantity']);
             $stmt->bindValue(5, $_POST['desc']);
             $stmt->bindValue(6, (isset($_POST['visible']) ? "1" : "0"), PDO::PARAM_INT);
