452ce93e89468a3fe9a4611c9d47443f449d0896
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-04-12 13:38:57 +0200

    Update product.php

--- product.php
+++ product.php
@@ -103,8 +103,8 @@ require("templates/header.php");
                     <h2 class="fw-blod">Wird oft zusammen gekauft</h2>
                     <div class="row row-cols-<?php if (isMobile()) print("1"); else print("3");?>">
                         <?php foreach ($products as $product1): ?>
-                            <div class="col my-2 d-flex mx-auto justify-content-center">
-                                <div class="card prodcard cbg2 d-flex justify-content-center">
+                            <div class="col my-2">
+                                <div class="card prodcard cbg2">
                                     <a href="product.php?id=<?=$product1['id']?>" class="stretched-link">
                                         <div class="card-body">
                                             <?php if (empty($product1['image'])) {
