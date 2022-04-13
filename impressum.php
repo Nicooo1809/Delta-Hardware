f8fae3b29c32e84e4dd2df958f53d253caf1d449
Nicooo <niggoapfel@gmail.com>
2022-04-13 08:15:15 +0200

    .

--- impressum.php
+++ impressum.php
@@ -2,7 +2,7 @@
 require_once("templates/header.php")
 ?>
 
-<div class="container">
+<div class="container mb-5">
     <div class="row no-gutter">
         <div class="col cbg impressum-breite">
             <h1>Impressum</h1>
