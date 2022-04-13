01e4ebb235e34ee2cc2db3632acabcb60ecc061b
Nicooo <niggoapfel@gmail.com>
2022-04-13 08:17:20 +0200

    Update impressum.php

--- impressum.php
+++ impressum.php
@@ -5,7 +5,7 @@ require_once("templates/header.php")
 <div class="container mb-5 mt-5">
     <div class="row no-gutter">
         <div class="col cbg impressum-breite">
-            <h1>Impressum</h1>
+            <h1 class="align-center">Impressum</h1>
             <br>
             <p>
                 Test
