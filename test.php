3c66932d8e426ede1b583dad8b47e1ad27dfa71e
Pflum <pflum@gmx.net>
2022-04-14 16:58:21 +0200

    .

--- test.php
+++ test.php
@@ -2,6 +2,10 @@
 require_once("php/functions.php");
 $user = require_once("templates/header.php");
 error('Test Error', 'fehler info');
+#$result = $stmt->fetch();
+#if ($result) {
+#    error('Database error', pdo_debugStrParams($stmt));
+#}
 ?>
 
 <?php
