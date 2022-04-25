23c0d61a695b59a40aed93bda4cc2c290bd13129
Pflum <pflum@gmx.net>
2022-04-25 11:09:14 +0200

    .

--- settings.php
+++ settings.php
@@ -80,7 +80,6 @@ if(isset($_GET['save'])) {
 	} else if($save == 'address') {
 		
 		if(isset($_POST['standardaddresse']) && !empty($_POST['standardaddresse'])) {
-			print_r($_POST);
 			$error_msg = "Bitte Addresse auswählen.";
 		} else {
 			$stmt = $pdo->prepare("UPDATE address SET default = 0, updated_at=NOW() WHERE default = 1 and user_id = ?");
