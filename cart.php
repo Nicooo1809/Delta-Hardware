5d07cfdfd7484149283d62eff38d86e7fb02c502
Nicooo <niggoapfel@gmail.com>
2022-04-25 10:49:54 +0200

    Update cart.php

--- cart.php
+++ cart.php
@@ -200,14 +200,14 @@ foreach ($products as $product) {
                                 <tr>
                                     <th scope="row" class="border-0">
                                         <div class="p-2 row">
-                                            <div class="col-md-6">
+                                            <div class="col-md-3">
                                                 <?php if (empty($product['image'])) {
                                                     print('<img src="images/image-not-found.png" width="150" class="img-fluid rounded shadow-sm" alt="' . $product['name'] . '">');
                                                 } else {
                                                     print('<img src="product_img/' . $product['image'] . '" width="150" class="img-fluid rounded shadow-sm" alt="' . $product['name'] . '">');
                                                 }?>
                                             </div>
-                                            <div class="col-md-6 text-wrap">
+                                            <div class="col-md-9 text-wrap">
                                                 <a href="product.php?id=<?=$product['product_id']?>" class="ctext d-inline-block align-middle text-wrap"><?=$product['name']?></a>
                                             </div>
                                         </div>
