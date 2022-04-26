c8b5ba4c3ce26058e722fb578353081901d7c7bb
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-04-26 09:35:12 +0200

    Update order.php

--- order.php
+++ order.php
@@ -112,68 +112,68 @@ $addresses = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <div class="container minheight100 py-3 px-3">
         <div class="row">
             <div class="py-3 px-3 cbg ctext rounded">
-                <?php if ($order[0]['sent']!=1): ?>
-                    <form action="?id=<?=$_GET['id']?>" method="post" class="">
-                        <div class="row d-flex justify-content-between">
-                <?php endif;?>
-                <div class="col-5">
-                    <h1>Bestellung</h1>
-                    <p><?=$total_products?> Produkt<?=($total_products>1 ? 'e':'')?></p>
-                    <p>Bestelldatum: <?=$order[0]['ordered_date']?></p>
-                    <?php if ($order[0]['sent']==1): ?>
-                        <p>Versanddatum: <?=$order[0]['sent_date']?></p>
-                    <?php endif ?>
-                </div>
-                <?php if ($order[0]['sent']!=1): ?>
-                    <div class="col-5 d-flex justify-content-end">
-                        <button class="btn btn-outline-danger me-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" aria-controls="offcanvas">Bestellung stornieren</button>
-                        <div class="offcanvas offcanvas-end cbg" data-bs-scroll="true" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel">
-                            <div class="offcanvas-header">
-                                <h2 class="offcanvas-title ctext" id="offcanvasLabel">Wirlich Löschen?</h2>
-                                <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                <form action="?id=<?=$_GET['id']?>" method="post" class="">
+                    <div class="row d-flex justify-content-between">
+                        <div class="col-5">
+                            <h1>Bestellung</h1>
+                            <p><?=$total_products?> Produkt<?=($total_products>1 ? 'e':'')?></p>
+                            <p>Bestelldatum: <?=$order[0]['ordered_date']?></p>
+                            <?php if ($order[0]['sent']==1): ?>
+                                <p>Versanddatum: <?=$order[0]['sent_date']?></p>
+                            <?php endif ?>
+                        </div>
+                        <?php if ($order[0]['sent']!=1): ?>
+                            <div class="col-5 d-flex justify-content-end">
+                                <button class="btn btn-outline-danger me-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" aria-controls="offcanvas">Bestellung stornieren</button>
+                                <div class="offcanvas offcanvas-end cbg" data-bs-scroll="true" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel">
+                                    <div class="offcanvas-header">
+                                        <h2 class="offcanvas-title ctext" id="offcanvasLabel">Wirlich Löschen?</h2>
+                                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                    </div>
+                                    <div class="offcanvas-body">
+                                        <div class="d-flex justify-content-center">
+                                            <button class="btn btn-outline-success mx-2" type="submit" name="action" value="del">Ja</button>
+                                            <button class="btn btn-outline-danger mx-2" type="button" data-bs-dismiss="offcanvas" aria-label="Close">Nein</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button type="submit" name="action" value="edit" class="btn btn-outline-success ms-2">Speichern</button>
                             </div>
-                            <div class="offcanvas-body">
-                                <div class="d-flex justify-content-center">
-                                    <button class="btn btn-outline-success mx-2" type="submit" name="action" value="del">Ja</button>
-                                    <button class="btn btn-outline-danger mx-2" type="button" data-bs-dismiss="offcanvas" aria-label="Close">Nein</button>
+                            <div class="row d-flex justify-content-between">
+                                <div class="col-5 mx-1">
+                                    <div class="input-group mb-3">
+                                        <label class="text-dark input-group-text" for="inputRechnugsaddresse">Rechnungsadresse</label>
+                                        <select class="form-select border-0 text-dark fw-bold" id="inputRechnugsaddresse" name="rechnugsaddresse">
+                                            <?php foreach ($addresses as $address): ?>
+                                                <?php if ($address['default'] == 1): ?>
+                                                    <option class="text-dark" value="<?=$address['id']?>" selected><?=$address['street']?> <?=$address['number']?> - <?=$address['PLZ']?>, <?=$address['city']?></option>
+                                                <?php else:?>
+                                                    <option class="text-dark" value="<?=$address['id']?>" ><?=$address['street']?> <?=$address['number']?> - <?=$address['PLZ']?>, <?=$address['city']?></option>
+                                                <?php endif; ?>
+                                            <?php endforeach; ?>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-5 mx-1">
+                                    <div class="input-group mb-3">
+                                        <label class="text-dark input-group-text" for="inputLieferaddresse">Lieferadresse</label>
+                                        <select class="form-select border-0 text-dark fw-bold" id="inputLieferaddresse" name="lieferaddresse">
+                                            <?php foreach ($addresses as $address): ?>
+                                                <?php if ($address['default'] == 1): ?>
+                                                    <option class="text-dark" value="<?=$address['id']?>" selected><?=$address['street']?> <?=$address['number']?> - <?=$address['PLZ']?>, <?=$address['city']?></option>
+                                                <?php else:?>
+                                                    <option class="text-dark" value="<?=$address['id']?>" ><?=$address['street']?> <?=$address['number']?> - <?=$address['PLZ']?>, <?=$address['city']?></option>
+                                                <?php endif; ?>
+                                            <?php endforeach; ?>
+                                        </select>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <button type="submit" name="action" value="edit" class="btn btn-outline-success ms-2">Speichern</button>
-                    </div>
-                </div>
-                    <div class="row d-flex justify-content-between">
-                        <div class="col-5 mx-1">
-                            <div class="input-group mb-3">
-                                <label class="text-dark input-group-text" for="inputRechnugsaddresse">Rechnungsadresse</label>
-                                <select class="form-select border-0 text-dark fw-bold" id="inputRechnugsaddresse" name="rechnugsaddresse">
-                                    <?php foreach ($addresses as $address): ?>
-                                        <?php if ($address['default'] == 1): ?>
-                                            <option class="text-dark" value="<?=$address['id']?>" selected><?=$address['street']?> <?=$address['number']?> - <?=$address['PLZ']?>, <?=$address['city']?></option>
-                                        <?php else:?>
-                                            <option class="text-dark" value="<?=$address['id']?>" ><?=$address['street']?> <?=$address['number']?> - <?=$address['PLZ']?>, <?=$address['city']?></option>
-                                        <?php endif; ?>
-                                    <?php endforeach; ?>
-                                </select>
+                    </form>
+                    <?php else: ?>
                             </div>
-                        </div>
-                        <div class="col-5 mx-1">
-                            <div class="input-group mb-3">
-                                <label class="text-dark input-group-text" for="inputLieferaddresse">Lieferadresse</label>
-                                <select class="form-select border-0 text-dark fw-bold" id="inputLieferaddresse" name="lieferaddresse">
-                                    <?php foreach ($addresses as $address): ?>
-                                        <?php if ($address['default'] == 1): ?>
-                                            <option class="text-dark" value="<?=$address['id']?>" selected><?=$address['street']?> <?=$address['number']?> - <?=$address['PLZ']?>, <?=$address['city']?></option>
-                                        <?php else:?>
-                                            <option class="text-dark" value="<?=$address['id']?>" ><?=$address['street']?> <?=$address['number']?> - <?=$address['PLZ']?>, <?=$address['city']?></option>
-                                        <?php endif; ?>
-                                    <?php endforeach; ?>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-                <?php else: ?>
+                        </form>
                 <div class="row mb-2">
                     <div class="col-6">
                         <h2>Rechnungsaddresse</h2>
