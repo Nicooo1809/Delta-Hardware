568d7d696e07dacb5759b486b81585a14dfabeae
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-04-26 09:25:58 +0200

    Update user.php

--- admin/user.php
+++ admin/user.php
@@ -102,15 +102,15 @@ if(isset($_POST['action'])) {
                     <div class="row d-flex justify-content-between">
                         <div class="col-6">
                             <div class="input-group py-2">
-                                <span class="input-group-text" for="inputVorname">Vorname</span>
+                                <span class="input-group-text" for="inputVorname" style="min-width: 150px;">Vorname</span>
                                 <input class="form-control" id="inputVorname" name="vorname" type="text" value="<?=$user1[0]['vorname']?>" required>
                             </div>
                             <div class="input-group py-2">
-                                <span class="input-group-text" for="inputNachname">Nachname</span>
+                                <span class="input-group-text" for="inputNachname" style="min-width: 150px;">Nachname</span>
                                 <input class="form-control" id="inputNachname" name="nachname" type="text" value="<?=$user1[0]['nachname']?>" required>
                             </div>
                             <div class="input-group py-2">    
-                                <span class="input-group-text" for="inputEmail">E-Mail</span>
+                                <span class="input-group-text" for="inputEmail" style="min-width: 150px;">E-Mail</span>
                                 <input class="form-control" id="inputEmail" name="email" type="email" value="<?=$user1[0]['email']?>" required>
                             </div>
                         </div>
