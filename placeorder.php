d3d4ab5f04eda5c8ede24ccbf3b9276679728842
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-04-29 16:10:31 +0200

    Update placeorder.php

--- placeorder.php
+++ placeorder.php
@@ -214,9 +214,9 @@ foreach ($products as $product) {
                 <h1>Bestellen</h1>
                 <p>Sie sind im Begriff folgende<?=($total_products>1 ? ' '.$total_products:'s')?> Produkt<?=($total_products>1 ? 'e':'')?> kostenpflichtig zu bestellen. Sind Sie Sicher?</p>
                 <form action="placeorder.php" method="post" class="">
-                    <div class="row d-flex justify-content-between">
-                        <div class="col-5 mx-1">
-                            <div class="input-group mb-3">
+                    <div class="row">
+                        <div class="my-3">
+                            <div class="input-group">
                                 <label class="text-dark input-group-text" for="inputRechnugsaddresse">Rechnungsadresse</label>
                                 <select class="form-select border-0 text-dark fw-bold" id="inputRechnugsaddresse" name="rechnugsaddresse">
                                     <?php foreach ($addresses as $address): ?>
@@ -229,8 +229,8 @@ foreach ($products as $product) {
                                 </select>
                             </div>
                         </div>
-                        <div class="col-5 mx-1">
-                            <div class="input-group mb-3">
+                        <div class="my-3">
+                            <div class="input-group">
                                 <label class="text-dark input-group-text" for="inputLieferaddresse">Lieferadresse</label>
                                 <select class="form-select border-0 text-dark fw-bold" id="inputLieferaddresse" name="lieferaddresse">
                                     <?php foreach ($addresses as $address): ?>
@@ -244,72 +244,29 @@ foreach ($products as $product) {
                             </div>
                         </div>
                     </div>
-                    <button class="btn btn-success mx-1" type="submit" name="confirm" value="yes">Kostenpflichtig bestellen</button>
-                    <button class="btn btn-danger mx-1" type="button" onclick="window.location.href = 'cart.php';">Abbrechen</button>
+                    <button class="btn btn-success my-3" type="submit" name="confirm" value="yes">Kostenpflichtig bestellen</button>
+                    <button class="btn btn-danger my-3" type="button" onclick="window.location.href = 'cart.php';">Abbrechen</button>
                 </form>
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <div class="ctext rounded">
-                                    <th scope="col" class="border-0">
-                                        <div class="p-2 px-3 text-uppercase ctext">Produkt</div>
-                                    </th>
-                                    <th scope="col" class="border-0 text-center">
-                                        <div class="p-2 px-3 text-uppercase ctext">Preis</div>
-                                    </th>
-                                    <th scope="col" class="border-0 text-center">
-                                        <div class="p-2 px-3 text-uppercase ctext">Menge</div>
-                                    </th>
-                                    <th scope="col" class="border-0 text-center" style="width: 10%;">
-                                    </th>
+                <div class="row row-cols-1">
+                    <?php foreach ($products as $product): ?>
+                        <div class="col">
+                            <div class="card mx-auto cbg">
+                                <div class="card-body">
+                                    <?php if (empty($product['image'])) {
+                                        print('<img src="images/image-not-found.png" class="card-img-top rounded mb-3" alt="' . $product['name'] . '">');
+                                    } else {
+                                        print('<img src="product_img/' . $product['image'] . '" class="card-img-top rounded mb-3" alt="' . $product['name'] . '">');
+                                    }?>
+                                    <h4 class="card-title name"><?=$product['name']?></h4>
+                                    <span class="card-text price">
+                                        Preis: &euro;<?=$product['price']?><br>
+                                        Menge: <?=$product['quantity']?>
+                                    </span>
                                 </div>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($products as $product): ?>
-                                <tr>
-                                    <th scope="row" class="border-0">
-                                        <div class="p-2">
-                                            <?php if (empty($product['image'])) {
-                                                print('<img src="images/image-not-found.png" width="150" class="img-fluid rounded shadow-sm" alt="' . $product['name'] . '">');
-                                            } else {
-                                                print('<img src="product_img/' . $product['image'] . '" width="150" class="img-fluid rounded shadow-sm" alt="' . $product['name'] . '">');
-                                            }?>
-                                            <div class="ms-3 d-inline-block align-middle">
-                                                <h5 class="mb-0"> 
-                                                    <a href="product.php?id=<?=$product['product_id']?>" class="ctext d-inline-block align-middle"><?=$product['name']?></a>
-                                                </h5>
-                                            </div>
-                                        </div>
-                                    </th>
-                                    <td class="border-0 align-middle text-center ctext">
-                                        <span><?=$product['price']?>&euro;</span>
-                                    </td>
-                                    <td class="border-0 align-middle text-center ctext">
-                                        <span><?=$product['quantity']?></span>
-                                    </td>
-                                    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-                                        <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
-                                            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                                        </symbol>
-                                    </svg>
-                                    <td class="border-0 align-middle text-center ctext">
-                                    <?=($product['maxquantity'] < $product['quantity'] ? '
-                                        <button type="button" class="btn btn-danger d-flex justify-content-center" data-bs-toggle="tooltip" data-bs-html="true" data-bs-placement="top" title="<h5>Hardware nicht vollständig auf Lager!</h5>">
-                                            <svg class="bi flex-shrink-0 me-0" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-                                        </button>' : "")?>
-                                        <script>
-                                            var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-                                            var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-                                                return new bootstrap.Tooltip(tooltipTriggerEl)
-                                            })
-                                        </script>
-                                    </td>
-                                </tr>
-                            <?php endforeach; ?>
-                        </tbody>
-                    </table>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
                 </div>         
                 <strong>Summe: <?=$summprice?>&euro;</strong>
             </div>
