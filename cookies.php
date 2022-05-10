7a8af477d875e8e13165c64faa7317afa511d7f2
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-05-10 19:18:17 +0200

    Update cookies.php

--- cookies.php
+++ cookies.php
@@ -17,7 +17,7 @@ require_once("templates/header.php");
                     <p class="fs-5 ctext cookie-p-text mb-1">- Speichern der Cookie-Einstellung</p>
                     <br>
                     <p class="fw-light fs-6 cookie-p-text ctext mb-5">Ihre Cookie-Einstellung wird gespeichert.</p>
-                    <a href="index"><button type="button" class="btn btn-primary">Zurück zur Startseite</button></a>
+                    <a href="index.php"><button type="button" class="btn btn-primary">Zurück zur Startseite</button></a>
                 </div>
             </div>
         </div>
