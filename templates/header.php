5e11d19447d086dd985b8331057339b5bccc2ae5
Pflum <pflum@gmx.net>
2022-05-10 20:56:16 +0200

    .

--- templates/header.php
+++ templates/header.php
@@ -36,7 +36,7 @@ $user1 = check_user(FALSE);
 <nav class="navbar header-header navbar-expand-lg navbar-<?php print(check_style());?> cbg ctext sticky-top">
     <div class="container-fluid">
         <a class="navbar-brand" href="/">
-            <?php include_once('/favicon.svg') ?>
+            <?php include_once('favicon.svg') ?>
             <!--<img src="/favicon.svg" class="navbar-icon">-->
         </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
