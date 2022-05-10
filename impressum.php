33e64889c60cd10944dd27370a579e9a07092820
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-05-10 17:31:15 +0200

    Update impressum.php

--- impressum.php
+++ impressum.php
@@ -3,7 +3,7 @@
 require_once("templates/header.php")
 ?>
 
-<div class="container mb-5 mt-5">
+<div class="container mb-5 mt-5 minheight100">
     <div class="row no-gutter">
         <div class="col cbg impressum-breite text-center">
             <h1 class="mt-2">Impressum (FIKTIVER ONLINESHOP)</h1>
