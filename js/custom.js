a16c5fc407b1775eb19be24eb4c134024114a3e9
Chaosjan44 <53968126+Chaosjan44@users.noreply.github.com>
2022-05-12 17:53:03 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -38,62 +38,65 @@ function toggleStyle() {
 }
 
 function setStyle() {
-  if (getCookie("style") == "custom") {
-    setCookie("style", "custom", 365);
-    if (document.querySelectorAll("link[href='/css/custom.css']").length > 0) {
-      document.querySelectorAll("link[href='/css/custom.css']")[0].disabled = false;
-    } else {
-      var head = document.getElementsByTagName('head')[0];
-      var style = document.createElement('link');
-      style.href = '/css/custom.css';
-      style.type = 'text/css';
-      style.rel = 'stylesheet';
-      head.append(style);
-    }
-    document.querySelectorAll("link[href='/css/dark.css']")[0].disabled = true;
-    document.querySelectorAll("link[href='/css/light.css']")[0].disabled = true;
-    if (document.querySelectorAll("link[href='/css/rainbow.css']").length > 0) {
-      document.querySelectorAll("link[href='/css/rainbow.css']")[0].disabled = true;
-    }
-  }
-  else if (getCookie("style") == "rainbow") {
-    setCookie("style", "rainbow", 365);
-    if (document.querySelectorAll("link[href='/css/rainbow.css']").length > 0) {
-      document.querySelectorAll("link[href='/css/rainbow.css']")[0].disabled = false;
-    } else {
-      var head = document.getElementsByTagName('head')[0];
-      var style = document.createElement('link');
-      style.href = '/css/rainbow.css';
-      style.type = 'text/css';
-      style.rel = 'stylesheet';
-      head.append(style);
-    }
-    document.querySelectorAll("link[href='/css/dark.css']")[0].disabled = true;
-    document.querySelectorAll("link[href='/css/light.css']")[0].disabled = true;
-    if (document.querySelectorAll("link[href='/css/custom.css']").length > 0) {
-      document.querySelectorAll("link[href='/css/custom.css']")[0].disabled = true;
-    }
-  }
-  else if (getCookie("style") == "dark") {
-    setCookie("style", "dark", 365);
-    document.querySelectorAll("link[href='/css/dark.css']")[0].disabled = false;
-    document.querySelectorAll("link[href='/css/light.css']")[0].disabled = true;
-    if (document.querySelectorAll("link[href='/css/custom.css']").length > 0) {
-      document.querySelectorAll("link[href='/css/custom.css']")[0].disabled = true;
-    }
-    if (document.querySelectorAll("link[href='/css/rainbow.css']").length > 0) {
-      document.querySelectorAll("link[href='/css/rainbow.css']")[0].disabled = true;
-    }
-  } else if (getCookie("style") == "light") {
-    setCookie("style", "light", 365);
-    document.querySelectorAll("link[href='/css/dark.css']")[0].disabled = true;
-    document.querySelectorAll("link[href='/css/light.css']")[0].disabled = false;
-    if (document.querySelectorAll("link[href='/css/custom.css']").length > 0) {
-      document.querySelectorAll("link[href='/css/custom.css']")[0].disabled = true;
-    }
-    if (document.querySelectorAll("link[href='/css/rainbow.css']").length > 0) {
-      document.querySelectorAll("link[href='/css/rainbow.css']")[0].disabled = true;
-    }
+  switch (getCookie("style")) {
+    case ("custom"):
+      setCookie("style", "custom", 365);
+      if (document.querySelectorAll("link[href='/css/custom.css']").length > 0) {
+        document.querySelectorAll("link[href='/css/custom.css']")[0].disabled = false;
+      } else {
+        var head = document.getElementsByTagName('head')[0];
+        var style = document.createElement('link');
+        style.href = '/css/custom.css';
+        style.type = 'text/css';
+        style.rel = 'stylesheet';
+        head.append(style);
+      }
+      document.querySelectorAll("link[href='/css/dark.css']")[0].disabled = true;
+      document.querySelectorAll("link[href='/css/light.css']")[0].disabled = true;
+      if (document.querySelectorAll("link[href='/css/rainbow.css']").length > 0) {
+        document.querySelectorAll("link[href='/css/rainbow.css']")[0].disabled = true;
+      }
+      break;
+    case ("rainbow"):
+      setCookie("style", "rainbow", 365);
+      if (document.querySelectorAll("link[href='/css/rainbow.css']").length > 0) {
+        document.querySelectorAll("link[href='/css/rainbow.css']")[0].disabled = false;
+      } else {
+        var head = document.getElementsByTagName('head')[0];
+        var style = document.createElement('link');
+        style.href = '/css/rainbow.css';
+        style.type = 'text/css';
+        style.rel = 'stylesheet';
+        head.append(style);
+      }
+      document.querySelectorAll("link[href='/css/dark.css']")[0].disabled = true;
+      document.querySelectorAll("link[href='/css/light.css']")[0].disabled = true;
+      if (document.querySelectorAll("link[href='/css/custom.css']").length > 0) {
+        document.querySelectorAll("link[href='/css/custom.css']")[0].disabled = true;
+      }
+      break;
+    case ("dark"):
+      setCookie("style", "dark", 365);
+      document.querySelectorAll("link[href='/css/dark.css']")[0].disabled = false;
+      document.querySelectorAll("link[href='/css/light.css']")[0].disabled = true;
+      if (document.querySelectorAll("link[href='/css/custom.css']").length > 0) {
+        document.querySelectorAll("link[href='/css/custom.css']")[0].disabled = true;
+      }
+      if (document.querySelectorAll("link[href='/css/rainbow.css']").length > 0) {
+        document.querySelectorAll("link[href='/css/rainbow.css']")[0].disabled = true;
+      }
+      break;
+    default:
+      setCookie("style", "light", 365);
+      document.querySelectorAll("link[href='/css/dark.css']")[0].disabled = true;
+      document.querySelectorAll("link[href='/css/light.css']")[0].disabled = false;
+      if (document.querySelectorAll("link[href='/css/custom.css']").length > 0) {
+        document.querySelectorAll("link[href='/css/custom.css']")[0].disabled = true;
+      }
+      if (document.querySelectorAll("link[href='/css/rainbow.css']").length > 0) {
+        document.querySelectorAll("link[href='/css/rainbow.css']")[0].disabled = true;
+      }
+      break;
   }
 }
 
