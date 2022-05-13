77323d09388e66b5f51e26d3da9cf9b1a69b6116
Pflum <pflum@gmx.net>
2022-05-13 18:36:17 +0200

    .

--- products.php
+++ products.php
@@ -34,15 +34,7 @@ if (isset($_GET["search"])) {
             $search_pieces = explode(" ", $_GET["search"]);
             foreach ($search_pieces as $index => $search_piece) {
                 if (!empty($search_piece) and $search_piece != '') {
-                    $search_piece = ' ' . $search_piece . ' ';
-                    if ($index == 0) {
-                        $search_piece = trim($search_piece);
-                        $search_piece = $search_piece . ' ';
-                    }
-                    if ($index == (count($search_pieces) - 1)) {
-                        $search_piece = trim($search_piece);
-                        $search_piece = ' ' . $search_piece;
-                    }
+                    $search_piece = trim($search_piece);
                     $search .= 'and lower(products.name) like lower("%' . $search_piece . '%") ';
                 }
             }
